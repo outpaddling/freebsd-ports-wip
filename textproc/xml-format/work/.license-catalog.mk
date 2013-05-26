d31c81c3fb7719a2c8e33be3ac2bd8b6be3578f9
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2013-05-26 13:46:56 +0000

    remove work dir

--- textproc/xml-format/work/.license-catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=BSD
-_LICENSE_NAME=BSD license
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF OSI COPYFREE
-_LICENSE_DISTFILES=xml-format-0.1.tar.gz
