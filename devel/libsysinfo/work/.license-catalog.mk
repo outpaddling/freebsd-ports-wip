028a9784809e458c0e139ba21064015bd4dc225c
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2013-06-15 17:19:22 +0000

    Add net/mtr-current

---
+++ devel/libsysinfo/work/.license-catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=BSD
+_LICENSE_NAME=BSD license
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI COPYFREE
+_LICENSE_DISTFILES=libsysinfo-0.0.2.tar.gz
