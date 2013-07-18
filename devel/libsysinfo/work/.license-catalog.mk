76974ee32a752312de084532f30170339723ae9b
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2013-07-18 16:49:04 +0000

    Remove extraneous work directory

--- devel/libsysinfo/work/.license-catalog.mk
+++
@@ -1,5 +0,0 @@
-_LICENSE=BSD
-_LICENSE_NAME=BSD license
-_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
-_LICENSE_GROUPS=FSF OSI COPYFREE
-_LICENSE_DISTFILES=libsysinfo-0.0.2.tar.gz
