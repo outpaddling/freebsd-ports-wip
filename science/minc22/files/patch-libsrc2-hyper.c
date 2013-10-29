7e8e9fc6f9328792c05a80c6bd487c6604f87de9
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2013-10-29 02:00:07 +0000

    Patches for minc22

--- science/minc22/files/patch-libsrc2-hyper.c
+++ science/minc22/files/patch-libsrc2-hyper.c
@@ -1,5 +1,5 @@
 --- libsrc2/hyper.c.orig	2012-09-24 10:19:31.000000000 -0500
-+++ libsrc2/hyper.c	2013-09-19 08:23:00.000000000 -0500
++++ libsrc2/hyper.c	2013-10-28 20:53:12.000000000 -0500
 @@ -766,7 +766,7 @@
              miicv_setint(icv, MI_ICV_DO_DIM_CONV, TRUE);
            break;
