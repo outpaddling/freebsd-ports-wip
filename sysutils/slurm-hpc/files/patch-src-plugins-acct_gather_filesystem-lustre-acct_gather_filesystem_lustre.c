5c9e242a9a87d8f3d7871bd7ed2cda5be964ca21
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2013-11-14 20:09:49 +0000

    Integrate slurm and openmpi

--- sysutils/slurm-hpc/files/patch-src-plugins-acct_gather_filesystem-lustre-acct_gather_filesystem_lustre.c
+++ sysutils/slurm-hpc/files/patch-src-plugins-acct_gather_filesystem-lustre-acct_gather_filesystem_lustre.c
@@ -1,5 +1,5 @@
 --- src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c.orig	2013-09-10 16:44:33.000000000 -0500
-+++ src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c	2013-09-24 21:21:23.000000000 -0500
++++ src/plugins/acct_gather_filesystem/lustre/acct_gather_filesystem_lustre.c	2013-11-14 10:23:02.000000000 -0600
 @@ -49,6 +49,8 @@
  #include <getopt.h>
  #include <netinet/in.h>
