6209ac6430b93bdc7567191eed94687cddb56996
outpaddling <outpaddling@b5e86e13-0e2a-4aa4-8c98-a58d979f8953>
2014-11-12 13:13:14 +0000

    Revert condor to old patch strategy

--- sysutils/condor/files/patch-src_python-bindings_secman.cpp
+++ sysutils/condor/files/patch-src_python-bindings_secman.cpp
@@ -1,6 +1,6 @@
 --- src/python-bindings/secman.cpp.orig	2014-09-30 16:17:32.000000000 -0500
-+++ src/python-bindings/secman.cpp	2014-11-11 18:13:02.000000000 -0600
-@@ -7,6 +7,20 @@
++++ src/python-bindings/secman.cpp	2014-11-12 07:07:41.000000000 -0600
+@@ -7,6 +7,24 @@
  # include <pyport.h>
  # endif
  
@@ -12,10 +12,14 @@
 + */
 +
 +#ifdef __FreeBSD__
-+#undef _BSD_SOURCE
++//#undef _BSD_SOURCE
 +//#undef _GNU_SOURCE
++#define dprintf _hide_dprintf
++#define getline _hide_getline
 +#include <Python.h>
-+#define _BSD_SOURCE
++#undef getline
++#undef dprintf
++//#define _BSD_SOURCE
 +#endif
 +
  #include "condor_common.h"
