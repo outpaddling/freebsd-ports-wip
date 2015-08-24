545558bb57ab5fc8b6a45c3e21f319487588b76f
outpaddling <jwbacon@tds.net>
2015-08-24 17:06:36 -0500

    Add new ports from unixdev wip

---
+++ biology/blasr-combined/files/patch-libcpp_pbdata_reads_HoleXY.hpp
@@ -0,0 +1,10 @@
+--- libcpp/pbdata/reads/HoleXY.hpp.orig	2015-08-15 09:22:58.000000000 -0500
++++ libcpp/pbdata/reads/HoleXY.hpp	2015-08-17 12:55:17.000000000 -0500
+@@ -2,6 +2,7 @@
+ #define _BLASR_HOLE_XY_HPP_
+ 
+ #include <stdlib.h>
++#include <stdint.h>
+ 
+ class HoleXY {
+ public:
---
+++ biology/blasr_libcpp/files/patch-pbdata_reads_HoleXY.hpp
@@ -0,0 +1,10 @@
+--- pbdata/reads/HoleXY.hpp.orig	2015-08-12 17:41:23 UTC
++++ pbdata/reads/HoleXY.hpp
+@@ -2,6 +2,7 @@
+ #define _BLASR_HOLE_XY_HPP_
+ 
+ #include <stdlib.h>
++#include <stdint.h>
+ 
+ class HoleXY {
+ public:
