606eabe1cfe96e9de2ac47a028ba12a148a8ed2c
outpaddling <jwbacon@tds.net>
2015-08-26 17:48:33 -0500

    Single quotes

--- biology/py-pbfalcon/files/patch-src_py_mains_run.py
+++ biology/py-pbfalcon/files/patch-src_py_mains_run.py
@@ -1,11 +1,11 @@
 --- src/py/mains/run.py.orig	2015-08-17 22:40:24.000000000 -0500
-+++ src/py/mains/run.py	2015-08-26 17:44:16.000000000 -0500
++++ src/py/mains/run.py	2015-08-26 17:48:16.000000000 -0500
 @@ -49,6 +49,9 @@
      We will have a better solution eventually.
      """
      script_fn = job_data["script_fn"]
 +    f = open(script_fn, 'w')
-+    f.write("#!/usr/bin/env bash\n\n")
++    f.write('#!/usr/bin/env bash\n\n')
 +    f.close()
      update_env_in_script(script_fn,
          ['PATH', 'PYTHONPATH', 'LD_LIBRARY_PATH'])
