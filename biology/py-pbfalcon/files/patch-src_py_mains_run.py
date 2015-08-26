745579a2436b927fa33a11af0b46c4972a91e820
outpaddling <jwbacon@tds.net>
2015-08-26 17:44:36 -0500

    Try another patch

--- biology/py-pbfalcon/files/patch-src_py_mains_run.py
+++ biology/py-pbfalcon/files/patch-src_py_mains_run.py
@@ -1,11 +1,12 @@
---- src/py/mains/run.py.orig	2015-08-26 17:32:02.000000000 -0500
-+++ src/py/mains/run.py	2015-08-26 17:34:55.000000000 -0500
-@@ -49,6 +49,8 @@
+--- src/py/mains/run.py.orig	2015-08-17 22:40:24.000000000 -0500
++++ src/py/mains/run.py	2015-08-26 17:44:16.000000000 -0500
+@@ -49,6 +49,9 @@
      We will have a better solution eventually.
      """
      script_fn = job_data["script_fn"]
-+    with open(script_fn, 'w') as ofs:
-+        ofs.write("#!/usr/bin/env bash\n\n")
++    f = open(script_fn, 'w')
++    f.write("#!/usr/bin/env bash\n\n")
++    f.close()
      update_env_in_script(script_fn,
          ['PATH', 'PYTHONPATH', 'LD_LIBRARY_PATH'])
      if job_type == "SGE":
