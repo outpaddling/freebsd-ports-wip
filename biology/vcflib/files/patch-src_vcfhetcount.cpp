0d0fbbb77f4c9302a3f69b8730e16826519d234e
outpaddling <jwbacon@tds.net>
2015-11-08 22:40:23 -0600

    Add patches to vcflib

---
+++ biology/vcflib/files/patch-src_vcfhetcount.cpp
@@ -0,0 +1,10 @@
+--- src/vcfhetcount.cpp.orig	2015-11-08 22:22:37.000000000 -0600
++++ src/vcfhetcount.cpp	2015-11-08 22:22:50.000000000 -0600
+@@ -2,6 +2,7 @@
+ #include "split.h"
+ #include <string>
+ #include <iostream>
++#include <getopt.h>
+ 
+ using namespace std;
+ using namespace vcflib;
