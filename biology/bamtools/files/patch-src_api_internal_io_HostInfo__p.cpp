cadd04806bff400d255a9722221add3e11f0789f
outpaddling <jwbacon@tds.net>
2015-11-11 09:43:47 -0600

    Clean up bamtools and add as a dep to ddocent

--- biology/bamtools/files/patch-src_api_internal_io_HostInfo__p.cpp
+++ biology/bamtools/files/patch-src_api_internal_io_HostInfo__p.cpp
@@ -1,5 +1,5 @@
---- src/api/internal/io/HostInfo_p.cpp.orig	2015-11-10 12:27:36.000000000 -0600
-+++ src/api/internal/io/HostInfo_p.cpp	2015-11-10 12:42:10.000000000 -0600
+--- src/api/internal/io/HostInfo_p.cpp.orig	2015-06-16 16:50:53 UTC
++++ src/api/internal/io/HostInfo_p.cpp
 @@ -11,6 +11,8 @@
  using namespace BamTools;
  using namespace BamTools::Internal;
--- biology/bamtools/files/patch-src_api_internal_io_TcpSocketEngine__unix__p.cpp
+++ biology/bamtools/files/patch-src_api_internal_io_TcpSocketEngine__unix__p.cpp
@@ -1,5 +1,5 @@
---- src/api/internal/io/TcpSocketEngine_unix_p.cpp.orig	2015-11-10 12:40:19.000000000 -0600
-+++ src/api/internal/io/TcpSocketEngine_unix_p.cpp	2015-11-10 12:40:33.000000000 -0600
+--- src/api/internal/io/TcpSocketEngine_unix_p.cpp.orig	2015-06-16 16:50:53 UTC
++++ src/api/internal/io/TcpSocketEngine_unix_p.cpp
 @@ -12,6 +12,8 @@
  using namespace BamTools;
  using namespace BamTools::Internal;
