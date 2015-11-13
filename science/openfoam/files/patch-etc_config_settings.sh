c7688d5e60bc41e2b5d29ebffa5138a1412d45d4
outpaddling <jwbacon@tds.net>
2015-11-13 15:08:06 -0600

    Patch assumption that sed == gsed

--- science/openfoam/files/patch-etc_config_settings.sh
+++ science/openfoam/files/patch-etc_config_settings.sh
@@ -1,5 +1,5 @@
 --- etc/config/settings.sh.orig	2015-11-03 04:30:23.000000000 -0600
-+++ etc/config/settings.sh	2015-11-12 20:54:19.000000000 -0600
++++ etc/config/settings.sh	2015-11-13 14:46:14.000000000 -0600
 @@ -65,6 +65,25 @@
  export WM_ARCH=`uname -s`
  
@@ -26,3 +26,12 @@
  Linux)
      WM_ARCH=linux
  
+@@ -392,7 +411,7 @@
+     # Use the system installed openmpi, get library directory via mpicc
+     export FOAM_MPI=openmpi-system
+ 
+-    libDir=`mpicc --showme:link | sed -e 's/.*-L\([^ ]*\).*/\1/'`
++    libDir=`mpicc --showme:link | gsed -e 's/.*-L\([^ ]*\).*/\1/'`
+ 
+     # Bit of a hack: strip off 'lib' and hope this is the path to openmpi
+     # include files and libraries.
