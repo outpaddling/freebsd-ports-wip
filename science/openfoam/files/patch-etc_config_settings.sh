09c5df15b84f76711db8639edcbd5a7b6e408389
outpaddling <jwbacon@tds.net>
2015-11-14 08:25:25 -0600

    Clean up

--- science/openfoam/files/patch-etc_config_settings.sh
+++ science/openfoam/files/patch-etc_config_settings.sh
@@ -1,5 +1,5 @@
 --- etc/config/settings.sh.orig	2015-11-03 04:30:23.000000000 -0600
-+++ etc/config/settings.sh	2015-11-13 15:55:18.000000000 -0600
++++ etc/config/settings.sh	2015-11-14 07:54:13.000000000 -0600
 @@ -65,6 +65,25 @@
  export WM_ARCH=`uname -s`
  
@@ -13,8 +13,8 @@
 +
 +    x86_64|powerpc64)
 +        export WM_COMPILER_ARCH=64
-+        export WM_CC='cc'
-+        export WM_CXX='c++'
++        export WM_CC='gcc48'
++        export WM_CXX='g++48'
 +        export WM_CFLAGS='-fPIC'
 +        export WM_CXXFLAGS='-fPIC'
 +        export WM_LDFLAGS=''
