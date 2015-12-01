3ffcf5b9cb707127591039d8adbd85ac94f63785
outpaddling <jwbacon@tds.net>
2015-12-01 12:17:00 -0600

    Force 64-bit arch on AMD64

--- science/openfoam/files/patch-etc_config_settings.sh
+++ science/openfoam/files/patch-etc_config_settings.sh
@@ -1,17 +1,19 @@
 --- etc/config/settings.sh.orig	2015-11-03 04:30:23.000000000 -0600
-+++ etc/config/settings.sh	2015-11-28 09:28:02.388860513 -0600
-@@ -65,6 +65,25 @@
++++ etc/config/settings.sh	2015-12-01 12:13:40.000000000 -0600
+@@ -65,6 +65,27 @@
  export WM_ARCH=`uname -s`
  
  case "$WM_ARCH" in
 +FreeBSD)
 +    WM_ARCH=FreeBSD
++
 +    case `uname -m` in
 +    i386|powerpc)
 +        export WM_ARCH_OPTION=32
 +        ;;
 +
-+    x86_64|powerpc64)
++    amd64|powerpc64)
++        export WM_ARCH_OPTION=64
 +        export WM_COMPILER_ARCH=64
 +        export WM_CC='gcc48'
 +        export WM_CXX='g++48'
@@ -26,7 +28,7 @@
  Linux)
      WM_ARCH=linux
  
-@@ -391,6 +410,7 @@
+@@ -391,6 +412,7 @@
  SYSTEMOPENMPI)
      # Use the system installed openmpi, get library directory via mpicc
      export FOAM_MPI=openmpi-system
