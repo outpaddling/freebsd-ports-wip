59738d30aec6dfe49ad8deef62883cdce9f06279
outpaddling <jwbacon@tds.net>
2015-12-01 18:29:57 -0600

    Clean up

--- science/openfoam/files/patch-etc_config_settings.sh
+++ science/openfoam/files/patch-etc_config_settings.sh
@@ -1,25 +1,27 @@
---- etc/config/settings.sh.orig	2015-11-03 04:30:23.000000000 -0600
-+++ etc/config/settings.sh	2015-12-01 12:13:40.000000000 -0600
-@@ -65,6 +65,27 @@
+--- etc/config/settings.sh.orig	2015-12-01 18:26:50.543756559 -0600
++++ etc/config/settings.sh	2015-12-01 18:28:08.219760182 -0600
+@@ -65,6 +65,29 @@ _foamAddMan()
  export WM_ARCH=`uname -s`
  
  case "$WM_ARCH" in
 +FreeBSD)
-+    WM_ARCH=FreeBSD
++    export WM_CC='gcc48'
++    export WM_CXX='g++48'
++    export WM_CFLAGS='-fPIC'
++    export WM_CXXFLAGS='-fPIC'
++    export WM_LDFLAGS='-L${LOCALBASE}/mpi/openmpi/lib'
 +
 +    case `uname -m` in
-+    i386|powerpc)
++    i386|powerpc|sparc)
++        export WM_ARCH=FreeBSD
 +        export WM_ARCH_OPTION=32
++        export WM_COMPILER_ARCH=32
 +        ;;
 +
-+    amd64|powerpc64)
++    amd64|powerpc64|sparc64)
++        export WM_ARCH=FreeBSD64
 +        export WM_ARCH_OPTION=64
 +        export WM_COMPILER_ARCH=64
-+        export WM_CC='gcc48'
-+        export WM_CXX='g++48'
-+        export WM_CFLAGS='-fPIC'
-+        export WM_CXXFLAGS='-fPIC'
-+        export WM_LDFLAGS='-L${LOCALBASE}/mpi/openmpi/lib'
 +        ;;
 +
 +    esac
@@ -28,7 +30,7 @@
  Linux)
      WM_ARCH=linux
  
-@@ -391,6 +412,7 @@
+@@ -391,6 +414,7 @@ case "$WM_MPLIB" in
  SYSTEMOPENMPI)
      # Use the system installed openmpi, get library directory via mpicc
      export FOAM_MPI=openmpi-system
