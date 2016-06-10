9929f174efef3cd5affb7f4bde466c0ab52d8b23
outpaddling <bacon4000@gmail.com>
2016-06-09 19:55:42 -0500

    Comment FreeBSD patches

--- sysutils/slurm-wlm/files/patch-src_plugins_task_affinity_affinity.c
+++ sysutils/slurm-wlm/files/patch-src_plugins_task_affinity_affinity.c
@@ -1,10 +1,11 @@
 --- src/plugins/task/affinity/affinity.c.orig	2016-05-03 17:41:59.000000000 -0500
-+++ src/plugins/task/affinity/affinity.c	2016-06-09 15:47:38.087977111 -0500
-@@ -291,12 +291,33 @@ int get_cpuset(cpu_set_t *mask, stepd_st
++++ src/plugins/task/affinity/affinity.c	2016-06-09 19:51:56.144588044 -0500
+@@ -291,12 +291,34 @@ int get_cpuset(cpu_set_t *mask, stepd_st
  	return false;
  }
  
-+#ifdef __FreeBSD__
++/* For sysctl() functions */
++#if defined(__FreeBSD__) || defined(__NetBSD__)
 +#include <sys/types.h>
 +#include <sys/sysctl.h>
 +#endif
@@ -15,7 +16,7 @@
  static bool _is_power_cpu(void)
  {
  	if (is_power == -1) {
-+#if defined(__FreeBSD__)
++#if defined(__FreeBSD__) || defined(__NetBSD__)
 +
 +		char    buffer[BUFFLEN+1];
 +		size_t  len = BUFFLEN;
@@ -35,13 +36,14 @@
  		char* _cpuinfo_path = "/proc/cpuinfo";
  		cpu_info_file = fopen(_cpuinfo_path, "r");
  		if (cpu_info_file == NULL) {
-@@ -313,6 +334,13 @@ static bool _is_power_cpu(void)
+@@ -313,6 +335,14 @@ static bool _is_power_cpu(void)
  			}
  		}
  		fclose(cpu_info_file);
 +
 +#else
 +
++/* Assuming other platforms don't support sysctlbyname() or /proc/cpuinfo */
 +#warning	"Power7 check not implemented for this platform."
 +	is_power = 0;
 +
@@ -49,7 +51,7 @@
  	}
  
  	if (is_power == 1)
-@@ -336,7 +364,11 @@ void reset_cpuset(cpu_set_t *new_mask, c
+@@ -336,7 +366,11 @@ void reset_cpuset(cpu_set_t *new_mask, c
  	if (slurm_getaffinity(1, sizeof(full_mask), &full_mask)) {
  		/* Try to get full CPU mask from process init */
  		CPU_ZERO(&full_mask);
@@ -61,7 +63,7 @@
  	}
  	CPU_ZERO(&newer_mask);
  	for (cur_offset = 0; cur_offset < CPU_SETSIZE; cur_offset++) {
-@@ -361,7 +393,10 @@ int slurm_setaffinity(pid_t pid, size_t 
+@@ -361,7 +395,10 @@ int slurm_setaffinity(pid_t pid, size_t 
  	int rval;
  	char mstr[1 + CPU_SETSIZE / 4];
  
@@ -73,11 +75,20 @@
  	rval = sched_setaffinity(pid, size, mask);
  #else
  	rval = sched_setaffinity(pid, mask);
-@@ -379,7 +414,10 @@ int slurm_getaffinity(pid_t pid, size_t 
+@@ -379,7 +416,19 @@ int slurm_getaffinity(pid_t pid, size_t 
  	char mstr[1 + CPU_SETSIZE / 4];
  
  	CPU_ZERO(mask);
 -#ifdef SCHED_GETAFFINITY_THREE_ARGS
++
++	/*
++	 * The FreeBSD cpuset API is a superset of the Linux API.
++	 * In addition to PIDs, it supports threads, interrupts,
++	 * jails, and potentially other objects.  The first two arguments
++	 * to cpuset_*etaffinity() below indicate that the third argument
++	 * is a PID.  -1 indicates the PID of the calling process.
++	 * Linux sched_*etaffinity() uses 0 for this.
++	 */
 +#ifdef __FreeBSD__
 +        rval = cpuset_getaffinity(CPU_LEVEL_WHICH, CPU_WHICH_PID,
 +				pid, size, mask);
