94784eee793fbffcec2d644c5971e769b8aa7def
outpaddling <bacon4000@gmail.com>
2016-07-14 10:17:49 -0500

    qterminal: Fix default font and window size

---
+++ x11/qterminal/files/patch-src_config.h
@@ -0,0 +1,15 @@
+--- src/config.h.orig	2014-10-22 03:13:14.000000000 -0500
++++ src/config.h	2016-07-14 10:12:11.679449000 -0500
+@@ -60,9 +60,9 @@
+ 
+ /* Some defaults for QTerminal application */
+ 
+-#define DEFAULT_WIDTH                  800
+-#define DEFAULT_HEIGHT                 600
+-#define DEFAULT_FONT                   "Monospace"
++#define DEFAULT_WIDTH                  820
++#define DEFAULT_HEIGHT                 597
++#define DEFAULT_FONT                   "Liberation Mono"
+ 
+ // ACTIONS
+ #define CLEAR_TERMINAL_SHORTCUT        "Ctrl+Shift+X"
