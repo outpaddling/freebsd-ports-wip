b027ea29cccf204a48acdbbb2c77f2bd90477430
outpaddling <bacon4000@gmail.com>
2016-07-15 16:45:05 -0500

    Clean up

--- x11/qterminal/files/patch-src_config.h
+++ x11/qterminal/files/patch-src_config.h
@@ -1,5 +1,5 @@
---- src/config.h.orig	2014-10-22 03:13:14.000000000 -0500
-+++ src/config.h	2016-07-14 10:12:11.679449000 -0500
+--- src/config.h.orig	2014-10-22 08:13:14 UTC
++++ src/config.h
 @@ -60,9 +60,9 @@
  
  /* Some defaults for QTerminal application */
