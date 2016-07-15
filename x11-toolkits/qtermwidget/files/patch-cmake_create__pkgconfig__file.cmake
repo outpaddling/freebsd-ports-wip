8f24c7d12d325a0ff17158a03ace48f9eaada439
outpaddling <bacon4000@gmail.com>
2016-07-15 16:17:26 -0500

    qterminwidget: Clean up

--- x11-toolkits/qtermwidget/files/patch-cmake_create__pkgconfig__file.cmake
+++ x11-toolkits/qtermwidget/files/patch-cmake_create__pkgconfig__file.cmake
@@ -1,9 +1,10 @@
---- cmake/create_pkgconfig_file.cmake.orig	2016-06-08 19:40:23 UTC
+--- cmake/create_pkgconfig_file.cmake.orig	2014-10-21 15:47:40 UTC
 +++ cmake/create_pkgconfig_file.cmake
-@@ -25,5 +25,5 @@ macro (create_pkgconfig_file name desc r
+@@ -25,5 +25,6 @@ macro (create_pkgconfig_file name desc r
              "\n"
          )
  
 -    install(FILES ${_pkgfname} DESTINATION ${CMAKE_INSTALL_LIBDIR}/pkgconfig)
++    # Use libdata rather than CMAKE_INSTALL_LIBDIR for pkgconfig on FreeBSD
 +    install(FILES ${_pkgfname} DESTINATION libdata/pkgconfig)
  endmacro()
