9a485a3f48d81345e4bec42656f4592b8aaea8f2
outpaddling <bacon4000@gmail.com>
2016-09-12 17:39:15 -0500

    unanimity: Clean up

--- biology/unanimity/files/patch-cmake_uny-gitsha1.cmake
+++ biology/unanimity/files/patch-cmake_uny-gitsha1.cmake
@@ -1,5 +1,5 @@
---- cmake/uny-gitsha1.cmake.orig	2016-09-07 19:13:09.950327612 -0500
-+++ cmake/uny-gitsha1.cmake	2016-09-07 19:13:17.424537305 -0500
+--- cmake/uny-gitsha1.cmake.orig	2016-08-12 12:47:37 UTC
++++ cmake/uny-gitsha1.cmake
 @@ -7,7 +7,7 @@ set(__find_git_sha1 YES)
  function(find_git_sha1 _GIT_SHA1)
      find_package(Git QUIET REQUIRED)
