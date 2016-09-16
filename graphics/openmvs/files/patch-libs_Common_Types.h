c7ccc6c1cd786c4e08489f8fb519b3fdf280f8f4
outpaddling <bacon4000@gmail.com>
2016-09-16 00:37:07 -0500

    openmvs: Patch for nonportable pthread code

--- graphics/openmvs/files/patch-libs_Common_Types.h
+++ graphics/openmvs/files/patch-libs_Common_Types.h
@@ -1,6 +1,28 @@
---- libs/Common/Types.h.orig	2016-04-06 20:59:29 UTC
-+++ libs/Common/Types.h
-@@ -331,7 +331,7 @@ FORCEINLINE T RANDOM() { return (T(1)/RA
+--- libs/Common/Types.h.orig	2016-04-06 15:59:29.000000000 -0500
++++ libs/Common/Types.h	2016-09-16 00:16:19.182458812 -0500
+@@ -180,6 +180,21 @@ namespace cv { namespace gpu = cuda; }
+ #  define __THREAD__ ((unsigned)GetCurrentThreadId())
+ # else
+ #  include <sys/syscall.h>
++// https://www.percona.com/forums/questions-discussions/percona-xtradb-cluster/24029-compilation-errors-sys_gettid-undeclared-expected-before-sigev_value
++#ifndef SYS_gettid
++    // i386: 224, ia64: 1105, amd64: 186, sparc 143
++    #ifdef __ia64__
++        #define SYS_gettid 1105
++    #elif __i386__
++        #define SYS_gettid 224
++    #elif __amd64__
++        #define SYS_gettid 186
++    #elif __sparc__
++        #define SYS_gettid 143
++    #else
++        #error define gettid for the arch
++    #endif
++#endif
+ #  define __THREAD__ ((unsigned)((pid_t)syscall(SYS_gettid)))
+ # endif
+ #endif
+@@ -331,7 +346,7 @@ FORCEINLINE T RANDOM() { return (T(1)/RA
  # define __BIG_ENDIAN 1
  # define __PDP_ENDIAN 2
  # define __BYTE_ORDER __LITTLE_ENDIAN
