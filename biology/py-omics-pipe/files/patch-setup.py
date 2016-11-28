c1ea395753369c553efd9933ae21a55e91c0cd86
outpaddling <bacon4000@gmail.com>
2016-11-28 14:27:54 -0600

    py-omics-pipe: Remove ez_setup hackware

---
+++ biology/py-omics-pipe/files/patch-setup.py
@@ -0,0 +1,10 @@
+--- setup.py.orig	2016-11-28 14:27:04.882289823 -0600
++++ setup.py	2016-11-28 14:27:08.711289109 -0600
+@@ -32,7 +32,6 @@ setup(
+     "sumatra>=0.5.2",
+     "PyYAML>=3.10",
+     "argparse",
+-    "ez_setup",
+     "setuptools",
+     "mercurial",
+     "hgapi",
