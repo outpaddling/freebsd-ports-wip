3a6843cd07a27aab3fc5c08bff578d0e2086e99d
outpaddling <bacon4000@gmail.com>
2017-01-12 10:06:57 -0600

    Add linux-bedops as a stopgap

--- biology/star/files/patch-source_SharedMemory.cpp
+++ biology/star/files/patch-source_SharedMemory.cpp
@@ -1,5 +1,5 @@
---- source/SharedMemory.cpp.orig	2016-06-10 11:54:47.702667727 -0500
-+++ source/SharedMemory.cpp	2016-06-10 11:56:08.108657804 -0500
+--- source/SharedMemory.cpp.orig	2016-05-10 21:41:15 UTC
++++ source/SharedMemory.cpp
 @@ -11,7 +11,7 @@
  #include <semaphore.h>
  #include <errno.h>
