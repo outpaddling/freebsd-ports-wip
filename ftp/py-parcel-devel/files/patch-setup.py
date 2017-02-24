51299b1aeb4193a9a664d474d3f5360324f5e49c
outpaddling <bacon4000@gmail.com>
2017-02-24 13:41:13 -0600

    py-parcel-devel: Work around missing stage dir, patch to allow newer deps from ports

--- ftp/py-parcel-devel/files/patch-setup.py
+++ ftp/py-parcel-devel/files/patch-setup.py
@@ -1,5 +1,5 @@
---- setup.py.orig	2015-07-15 08:52:20.000000000 -0500
-+++ setup.py	2017-02-17 11:34:47.515956000 -0600
+--- setup.py.orig	2016-05-26 08:55:22.000000000 -0500
++++ setup.py	2017-02-24 13:38:44.023975000 -0600
 @@ -11,8 +11,8 @@ def parcel_build(command_subclass):
  
      def parcel_run(self):
@@ -11,3 +11,22 @@
          except Exception as e:
              logging.error(
                  "Unable to build UDT library: {}".format(e))
+@@ -60,12 +60,12 @@ setup(
+         'develop': ParcelDevelop,
+     },
+     install_requires=[
+-        'requests==2.5.1',
+-        'progressbar==2.3',
+-        'Flask==0.10.1',
+-        'intervaltree==2.0.4',
+-        'termcolor==1.1.0',
+-        'cmd2==0.6.8',
++        'requests>=2.5.1',
++        'progressbar>=2.3',
++        'Flask>=0.10.1',
++        'intervaltree>=2.0.4',
++        'termcolor>=1.1.0',
++        'cmd2>=0.6.8',
+     ],
+     package_data={
+         "parcel": [
