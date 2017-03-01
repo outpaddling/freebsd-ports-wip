8657a5f189406166aba7ff20decc97e34e4157a9
outpaddling <bacon4000@gmail.com>
2017-03-01 10:44:25 -0600

    bowtie2: Clean up

--- biology/bowtie2/files/patch-aligner__result.cpp
+++ biology/bowtie2/files/patch-aligner__result.cpp
@@ -1,5 +1,5 @@
---- aligner_result.cpp.orig	2017-02-19 18:40:54.398991000 -0600
-+++ aligner_result.cpp	2017-02-19 18:41:14.093250000 -0600
+--- aligner_result.cpp.orig	2016-12-30 15:40:06 UTC
++++ aligner_result.cpp
 @@ -1130,7 +1130,7 @@ bool AlnFlags::printYF(BTString& o, bool
  	else if(!nfilt_  ) flag = "NS";
  	else if(!scfilt_ ) flag = "SC";
