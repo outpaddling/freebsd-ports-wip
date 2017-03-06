98fe7d60c2793199f3fae384c08f8065cd20ff04
outpaddling <bacon4000@gmail.com>
2017-03-06 12:03:12 -0600

    sumaclust: Fix Makefile to respect CC

--- biology/sumaclust/files/patch-global.mk
+++ biology/sumaclust/files/patch-global.mk
@@ -1,15 +1,31 @@
---- global.mk.orig	2015-07-16 12:36:45 UTC
-+++ global.mk
-@@ -12,6 +12,9 @@ LIBUTILS = ./sumalibs/libutils/libutils.
- CC=gcc
- LDFLAGS=
+--- global.mk.orig	2015-07-16 07:36:45.000000000 -0500
++++ global.mk	2017-03-06 11:56:45.503093472 -0600
+@@ -9,15 +9,18 @@ LIBLCS   = ./sumalibs/liblcs/liblcs.a
+ LIBFILE  = ./sumalibs/libfile/libfile.a
+ LIBUTILS = ./sumalibs/libutils/libutils.a
+ 
+-CC=gcc
+-LDFLAGS=
++CC?=gcc
++#LDFLAGS=
  
 +MKDIR	?= mkdir
 +INSTALL	?= install
 +STRIP	?= strip
  
- ifeq ($(CC),gcc)
-         CFLAGS = -O3 -s -DOMP_SUPPORT -fopenmp -w
+-ifeq ($(CC),gcc)
+-        CFLAGS = -O3 -s -DOMP_SUPPORT -fopenmp -w
+-else
+-        CFLAGS = -O3 -w
+-endif
++#ifeq ($(CC),gcc)
++        CFLAGS += -s -DOMP_SUPPORT -fopenmp -w
++#else
++        #CFLAGS = -O3 -w
++#endif
+ 
+ 
+ default: all
 @@ -42,4 +45,4 @@ default: all
  	$(MAKE) -C ./sumalibs/libfile
  
