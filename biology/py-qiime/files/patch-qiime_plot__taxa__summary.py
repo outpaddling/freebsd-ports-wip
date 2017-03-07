c044ad17235f8481169f8b18d8153f6de63e3b20
outpaddling <bacon4000@gmail.com>
2017-03-06 22:50:43 -0600

    py-qiime: Fix dep plists, add shebangfix

---
+++ biology/py-qiime/files/patch-qiime_plot__taxa__summary.py
@@ -0,0 +1,9 @@
+--- qiime/plot_taxa_summary.py.orig	2017-03-06 22:42:41.911611000 -0600
++++ qiime/plot_taxa_summary.py	2017-03-06 22:42:45.770973000 -0600
+@@ -1,5 +1,5 @@
+-from __future__ import division
+ #!/usr/bin/env python
++from __future__ import division
+ # file plot_taxa_summary.py
+ 
+ __author__ = "Jesse Stobmaugh"
