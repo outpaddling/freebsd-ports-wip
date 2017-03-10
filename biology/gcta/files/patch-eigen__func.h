4ecf2a117c13a654e99c5415eba66126bd7c9576
outpaddling <bacon4000@gmail.com>
2017-03-10 10:40:31 -0600

    gcta: Patch out MKL requirement

---
+++ biology/gcta/files/patch-eigen__func.h
@@ -0,0 +1,15 @@
+--- eigen_func.h.orig	2017-03-10 10:33:00.412167000 -0600
++++ eigen_func.h	2017-03-10 10:33:17.483478000 -0600
+@@ -12,9 +12,9 @@
+ #ifndef _EIGENFUNC_H
+ #define _EIGENFUNC_H
+ 
+-#ifndef EIGEN_USE_MKL_ALL
+-#define EIGEN_USE_MKL_ALL
+-#endif
++//#ifndef EIGEN_USE_MKL_ALL
++//#define EIGEN_USE_MKL_ALL
++//#endif
+ 
+ #include "CommFunc.h"
+ #include "StatFunc.h"
---
+++ biology/gcta/files/patch-gcta.h
@@ -0,0 +1,26 @@
+--- gcta.h.orig	2017-03-10 10:33:39.109610000 -0600
++++ gcta.h	2017-03-10 10:35:10.788255000 -0600
+@@ -17,9 +17,9 @@
+ #define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
+ #endif
+ 
+-#ifndef EIGEN_USE_MKL_ALL
+-#define EIGEN_USE_MKL_ALL
+-#endif
++//#ifndef EIGEN_USE_MKL_ALL
++//#define EIGEN_USE_MKL_ALL
++//#endif
+ 
+ #include "CommFunc.h"
+ #include "StrFunc.h"
+@@ -36,8 +36,8 @@
+ #include <unsupported/Eigen/SparseExtra>
+ #include <unsupported/Eigen/IterativeSolvers>
+ #include <omp.h>
+-#include <mkl_cblas.h>
+-#include <mkl_lapack.h>
++#include <cblas.h>
++//#include <lapack.h>
+ 
+ using namespace Eigen;
+ using namespace std;
