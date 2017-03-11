dfcea8f2aeff7d47a7b1c9ebae44510f316168f7
outpaddling <bacon4000@gmail.com>
2017-03-11 14:13:26 -0600

    gcta: Switch to lapacke from lapack++

--- biology/gcta/files/patch-gcta.h
+++ biology/gcta/files/patch-gcta.h
@@ -1,5 +1,5 @@
 --- gcta.h.orig	2016-06-21 22:11:01.000000000 -0500
-+++ gcta.h	2017-03-11 09:07:25.340446000 -0600
++++ gcta.h	2017-03-11 13:53:26.196740000 -0600
 @@ -17,9 +17,9 @@
  #define EIGEN_YES_I_KNOW_SPARSE_MODULE_IS_NOT_STABLE_YET
  #endif
@@ -13,16 +13,17 @@
  
  #include "CommFunc.h"
  #include "StrFunc.h"
-@@ -36,8 +36,10 @@
+@@ -36,8 +36,11 @@
  #include <unsupported/Eigen/SparseExtra>
  #include <unsupported/Eigen/IterativeSolvers>
  #include <omp.h>
 -#include <mkl_cblas.h>
 -#include <mkl_lapack.h>
 +#include <cblas.h>
-+#include <blaspp.h>
-+#include <lapackpp.h>
-+#include <lapackd.h>
++#include <lapacke.h>
++// Looks like openblas, cblas and lapacke are all that's needed
++//#include <blaspp.h>
++//#include <lapackpp.h>
  
  using namespace Eigen;
  using namespace std;
