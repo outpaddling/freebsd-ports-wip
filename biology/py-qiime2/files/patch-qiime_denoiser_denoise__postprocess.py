4e62a3ab9e8cb6992fb37a8fb4fb9cb183f93fac
outpaddling <bacon4000@gmail.com>
2017-04-02 19:00:30 -0500

    Auto-updated by remake

--- biology/py-qiime2/files/patch-qiime_denoiser_denoise__postprocess.py
+++
@@ -1,8 +0,0 @@
---- qiime/denoiser/denoise_postprocess.py.orig	2017-03-06 22:41:03.323596000 -0600
-+++ qiime/denoiser/denoise_postprocess.py	2017-03-06 22:41:09.050813000 -0600
-@@ -1,4 +1,4 @@
-- #!/usr/bin/env python
-+#!/usr/bin/env python
- 
- __author__ = "Jens Reeder"
- __copyright__ = "Copyright 2011, The QIIME Project"
