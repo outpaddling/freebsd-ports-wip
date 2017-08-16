327ec29b4d94dd3199980fc31e87d619f5991129
outpaddling <bacon4000@gmail.com>
2017-08-15 22:27:47 -0500

    epacts: Clean up

--- epacts/files/patch-src_FreqTable.h
+++ epacts/files/patch-src_FreqTable.h
@@ -1,5 +1,5 @@
---- src/FreqTable.h.orig	2017-08-15 21:07:23.727334208 -0500
-+++ src/FreqTable.h	2017-08-15 21:08:13.064328811 -0500
+--- src/FreqTable.h.orig	2017-02-22 08:30:03 UTC
++++ src/FreqTable.h
 @@ -15,7 +15,7 @@ public:
      };
      void remove(const T& t) {
