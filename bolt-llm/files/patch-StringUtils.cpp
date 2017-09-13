a80cef0d14145bd04155513ef97db9b1fb8cd4aa
outpaddling <bacon4000@gmail.com>
2017-09-12 23:59:06 -0500

    bolt-llm: Add missing header

---
+++ bolt-llm/files/patch-StringUtils.cpp
@@ -0,0 +1,10 @@
+--- StringUtils.cpp.orig	2017-09-12 23:16:25.540892000 -0500
++++ StringUtils.cpp	2017-09-12 23:17:33.415369000 -0500
+@@ -22,6 +22,7 @@
+ #include <cstdio>
+ #include <iostream>
+ #include <sstream>
++#include <sys/types.h>	// uint
+ 
+ #include "StringUtils.hpp"
+ 
