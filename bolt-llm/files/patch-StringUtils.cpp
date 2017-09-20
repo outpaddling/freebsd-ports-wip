ffc32c5e87aef12b93d2dc7bdfdcd49087182868
outpaddling <bacon4000@gmail.com>
2017-09-20 05:31:19 -0500

    bolt-llm: Clean up

--- bolt-llm/files/patch-StringUtils.cpp
+++ bolt-llm/files/patch-StringUtils.cpp
@@ -1,5 +1,5 @@
---- StringUtils.cpp.orig	2017-09-12 23:16:25.540892000 -0500
-+++ StringUtils.cpp	2017-09-12 23:17:33.415369000 -0500
+--- StringUtils.cpp.orig	2017-08-02 23:43:24 UTC
++++ StringUtils.cpp
 @@ -22,6 +22,7 @@
  #include <cstdio>
  #include <iostream>
