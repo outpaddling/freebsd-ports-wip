2eaf19e4148c265b942bfb196efb62fec2493f7c
outpaddling <bacon4000@gmail.com>
2017-11-08 18:57:15 -0600

    bolt-lmm: Fix compiler errors

--- bolt-lmm/files/patch-FileUtils.cpp
+++ bolt-lmm/files/patch-FileUtils.cpp
@@ -1,6 +1,16 @@
---- FileUtils.cpp.orig	2017-09-20 10:29:18 UTC
-+++ FileUtils.cpp
-@@ -275,7 +275,7 @@ namespace FileUtils {
+--- FileUtils.cpp.orig	2017-08-02 18:43:24.000000000 -0500
++++ FileUtils.cpp	2017-11-08 18:56:31.526411000 -0600
+@@ -187,7 +187,8 @@ namespace FileUtils {
+   }
+ 
+   AutoGzIfstream::operator bool() const {
+-    return boost_in;
++    // Guessing at intent here
++    return ! boost_in.empty();
+   }
+ 
+   AutoGzIfstream& AutoGzIfstream::read(char *s, std::streamsize n) {
+@@ -275,7 +276,7 @@ namespace FileUtils {
      uint Layout = (flags>>2)&0xf; cout << "Layout: " << Layout << endl;
      assert(Layout==1 || Layout==2); // REQUIRE Layout==1 or Layout==2
  
--- bolt-lmm/files/patch-StringUtils.cpp
+++ bolt-lmm/files/patch-StringUtils.cpp
@@ -1,5 +1,5 @@
---- StringUtils.cpp.orig	2017-08-02 23:43:24 UTC
-+++ StringUtils.cpp
+--- StringUtils.cpp.orig	2017-08-02 18:43:24.000000000 -0500
++++ StringUtils.cpp	2017-11-08 18:42:31.465273000 -0600
 @@ -22,6 +22,7 @@
  #include <cstdio>
  #include <iostream>
@@ -8,3 +8,12 @@
  
  #include "StringUtils.hpp"
  
+@@ -102,7 +103,7 @@ namespace StringUtils {
+       string prefix, suffix;
+       if (str[0] != RANGE_DELIMS[0]) prefix = tokens[0];
+       if (str[str.length()-1] != RANGE_DELIMS[2]) suffix = tokens.back();
+-      int start = stoi(tokens[startInd]), end = stoi(tokens[endInd]);
++      int start = StringUtils::stoi(tokens[startInd]), end = StringUtils::stoi(tokens[endInd]);
+       if (start > end+1 || end > start+1000000) {
+ 	cerr << "ERROR: Invalid range in template string: " << str << endl;
+ 	cerr << "  Start: " << start << endl;
