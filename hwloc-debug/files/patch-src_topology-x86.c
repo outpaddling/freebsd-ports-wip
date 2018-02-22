c18db96f93a06820808a3495cff3446ab4236bfc
outpaddling <bacon4000@gmail.com>
2018-02-22 11:44:24 -0600

    hwloc-debug: New patch that appears to fix the issue

--- hwloc-debug/files/patch-src_topology-x86.c
+++ hwloc-debug/files/patch-src_topology-x86.c
@@ -1,43 +1,17 @@
---- src/topology-x86.c.orig	2017-04-19 10:19:38.000000000 -0500
-+++ src/topology-x86.c	2018-01-14 11:19:18.712933000 -0600
-@@ -248,6 +248,9 @@ static void look_proc(struct hwloc_backe
-   if (cpuid_type != intel && has_topoext(features)) {
-     unsigned apic_id, node_id, nodes_per_proc;
- 
-+    /* the code below doesn't want any other cache yet */
-+    assert(!infos->numcaches);
-+
-     eax = 0x8000001e;
-     hwloc_x86_cpuid(&eax, &ebx, &ecx, &edx);
-     infos->apicid = apic_id = eax;
-@@ -355,6 +358,9 @@ static void look_proc(struct hwloc_backe
-    */
-   if (cpuid_type != amd && highest_cpuid >= 0x04) {
-     unsigned level;
-+
-+    unsigned oldnumcaches = infos->numcaches; /* in case we got caches above */
-+
-     for (cachenum = 0; ; cachenum++) {
-       unsigned type;
-       eax = 0x04;
-@@ -384,7 +390,8 @@ static void look_proc(struct hwloc_backe
-       }
-     }
- 
--    cache = infos->cache = malloc(infos->numcaches * sizeof(*infos->cache));
-+    infos->cache = realloc(infos->cache, infos->numcaches * sizeof(*infos->cache));
-+    cache = &infos->cache[oldnumcaches];
- 
-     for (cachenum = 0; ; cachenum++) {
-       unsigned long linesize, linepart, ways, sets;
-@@ -1040,6 +1047,10 @@ int hwloc_look_x86(struct hwloc_backend 
-   eax = 0x00;
-   hwloc_x86_cpuid(&eax, &ebx, &ecx, &edx);
-   highest_cpuid = eax;
-+  /* printf("got vendor %c%c%c%c%c%c%c%c%c%c%c%c\n",
-+	 ebx&0xff, (ebx>>8)&0xff, (ebx>>16)&0xff, (ebx>>24)&0xff,
-+	 edx&0xff, (edx>>8)&0xff, (edx>>16)&0xff, (edx>>24)&0xff,
-+	 ecx&0xff, (ecx>>8)&0xff, (ecx>>16)&0xff, (ecx>>24)&0xff); */
-   if (ebx == INTEL_EBX && ecx == INTEL_ECX && edx == INTEL_EDX)
-     cpuid_type = intel;
-   if (ebx == AMD_EBX && ecx == AMD_ECX && edx == AMD_EDX)
+--- src/topology-x86.c.orig	2017-04-19 15:19:38 UTC
++++ src/topology-x86.c
+@@ -997,7 +997,14 @@ int hwloc_look_x86(struct hwloc_backend *backend, int 
+   unsigned features[10] = { 0 };
+   struct procinfo *infos = NULL;
+   enum cpuid_type cpuid_type = unknown;
++#if defined HWLOC_FREEBSD_SYS && defined HAVE_CPUSET_SETID \
++ && __FreeBSD_version >= 1101000 && __FreeBSD_version < 1102000
++  /* Buffer overrun in cpuset_getid in FreeBSD 11.1 */
++  hwloc_x86_os_state_t os_state[2];
++#define os_state os_state[0]
++#else
+   hwloc_x86_os_state_t os_state;
++#endif
+   struct hwloc_binding_hooks hooks;
+   struct hwloc_topology_support support;
+   struct hwloc_topology_membind_support memsupport __hwloc_attribute_unused;
