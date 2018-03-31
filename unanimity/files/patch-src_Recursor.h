deaa7016711b5d8c01f97a65f187b036fddf97ae
outpaddling <bacon4000@gmail.com>
2018-03-31 13:41:19 -0500

    unanimity: Upgrade to latest commit 2018-03-07 and clean up

--- unanimity/files/patch-src_Recursor.h
+++ unanimity/files/patch-src_Recursor.h
@@ -1,8 +1,8 @@
 # Type mismatch on 11.0 i386
---- src/Recursor.h.orig	2016-09-12 19:55:43 UTC
+--- src/Recursor.h.orig	2018-03-07 23:10:34 UTC
 +++ src/Recursor.h
-@@ -728,7 +728,7 @@ size_t Recursor<Derived>::FillAlphaBeta(
-     size_t J = tpl_->Length();
+@@ -708,7 +708,7 @@ size_t Recursor<Derived>::FillAlphaBeta(
+     size_t J = tpl.Length();
      int flipflops = 0;
      size_t maxSize =
 -        std::max(100ul, static_cast<size_t>(0.5 + REBANDING_THRESHOLD * (I + 1) * (J + 1)));
