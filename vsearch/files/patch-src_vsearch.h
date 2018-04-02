e61e19fdbd72c3e428c5d2e849bec376d24bad68
outpaddling <bacon4000@gmail.com>
2018-04-01 23:40:01 -0500

    vsearch: Upgrade to 2.7.1

--- vsearch/files/patch-src_vsearch.h
+++ vsearch/files/patch-src_vsearch.h
@@ -1,14 +1,59 @@
---- src/vsearch.h.orig	2017-03-10 16:26:54 UTC
+--- src/vsearch.h.orig	2018-02-16 17:08:01 UTC
 +++ src/vsearch.h
-@@ -116,6 +116,11 @@
+@@ -119,30 +119,40 @@
+ 
+ #ifdef _WIN32
+ 
+-#define PROG_OS "win"
+-#include <windows.h>
+-#include <psapi.h>
++# define PROG_OS "win"
++# include <windows.h>
++# include <psapi.h>
+ 
+-#else
++#else	// Any Unix
+ 
+-#ifdef __APPLE__
++# ifdef __APPLE__
+ 
+-#define PROG_OS "macos"
+-#include <sys/sysctl.h>
++#  define PROG_OS "macos"
++#  include <sys/sysctl.h>
+ 
+-#else
++# else	// Not Apple
+ 
+-#ifdef __linux__
+-#define PROG_OS "linux"
+-#else
+-#define PROG_OS "unknown"
+-#endif
++#  ifdef __linux__
++#   define PROG_OS "linux"
+ 
+-#include <sys/sysinfo.h>
++#  else	// Not Apple or Linux
+ 
+-#endif
++#   ifdef __FreeBSD__
++#    define PROG_OS "freebsd"
+ 
+-#include <sys/resource.h>
++#   else	// None of the above
++
++#    define PROG_OS "unknown"
++
++#   endif
++
++#   include <sys/sysinfo.h>
++ 
++#  endif
++
++# endif
++
++# include <sys/resource.h>
  
  #endif
  
-+#ifdef __FreeBSD__
-+#define PROG_OS "freebsd"
-+#include <sys/resource.h>
-+#endif
-+
- #ifdef __APPLE__
- #define PROG_OS "osx"
- #include <sys/resource.h>
