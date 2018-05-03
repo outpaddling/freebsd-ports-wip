c7d2d2094f078fdc6e9ca19ad1a8b9c954dead7e
outpaddling <bacon4000@gmail.com>
2018-05-03 09:31:09 -0500

    plink-ng: Successful build with patched Makefile

---
+++ plink-ng/files/patch-plink2__base.h
@@ -0,0 +1,11 @@
+--- plink2_base.h.orig	2018-05-03 13:55:25 UTC
++++ plink2_base.h
+@@ -433,7 +433,7 @@ typedef uint32_t BoolErr;
+ #  define FOPEN_RB "r"
+ #  define FOPEN_WB "w"
+ #  define FOPEN_AB "a"
+-#  ifdef __APPLE__
++#  if defined(__APPLE__) || defined(__FreeBSD__)
+ #    define fread_unlocked fread
+ #    define fwrite_unlocked fwrite
+ #  endif
