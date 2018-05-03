13904f464b9d7960e06b05953b46c878de7e2d18
outpaddling <bacon4000@gmail.com>
2018-05-03 10:34:21 -0500

    plink: Clean up

--- plink/files/patch-plink__common.h
+++ plink/files/patch-plink__common.h
@@ -1,5 +1,5 @@
---- plink_common.h.orig	2017-08-30 22:37:30.805919000 -0500
-+++ plink_common.h	2017-08-30 22:37:47.253002000 -0500
+--- plink_common.h.orig	2017-08-27 17:35:52 UTC
++++ plink_common.h
 @@ -116,8 +116,8 @@
      // (may break on gcc <3.0?  but that shouldn't matter anymore)
      // tried defining GCC_VERSION, but that didn't always work
