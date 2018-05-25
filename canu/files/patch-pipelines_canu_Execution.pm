03fe245f8d47f0b33ffaa685f6e28c046d4d342c
outpaddling <bacon4000@gmail.com>
2018-05-25 16:47:37 -0500

    canu: Test patch to limit simultaneous jobs

---
+++ canu/files/patch-pipelines_canu_Execution.pm
@@ -0,0 +1,23 @@
+--- pipelines/canu/Execution.pm.orig	2018-02-27 13:46:07 UTC
++++ pipelines/canu/Execution.pm
+@@ -303,10 +303,6 @@ sub skipStage ($$@) {
+ sub getInstallDirectory () {
+     my $installDir = $FindBin::RealBin;
+ 
+-    if ($installDir =~ m!^(.*)/\w+-\w+/bin$!) {
+-        $installDir = $1;
+-    }
+-
+     return($installDir);
+ }
+ 
+@@ -727,6 +723,9 @@ sub buildGridArray ($$$$) {
+ 
+     $opt =~ s/ARRAY_NAME/$name/g;        #  Replace ARRAY_NAME with 'job name'
+     $opt =~ s/ARRAY_JOBS/$bgn-$end/g;    #  Replace ARRAY_JOBS with 'bgn-end'
++    if ( defined(getGlobal("gridEngineJobLimit")) ) {
++        $opt = $opt . '%' . getGlobal("gridEngineJobLimit");
++    }
+ 
+     return($opt, $off);
+ }
