7db711d56c0a1d121ee636ed24fcdb4e5e77aefe
outpaddling <bacon4000@gmail.com>
2018-05-27 18:16:50 -0500

    canu: Initialize slurmCormhapTaskLimit

--- canu/files/patch-pipelines_canu_Defaults.pm
+++ canu/files/patch-pipelines_canu_Defaults.pm
@@ -1,9 +1,12 @@
 --- pipelines/canu/Defaults.pm.orig	2018-02-27 13:46:07 UTC
 +++ pipelines/canu/Defaults.pm
-@@ -637,6 +637,9 @@ sub setExecDefaults ($$) {
+@@ -637,6 +637,12 @@ sub setExecDefaults ($$) {
      my $tag         = shift @_;
      my $name        = shift @_;
  
++    $global{"slurmCormhapTaskLimit"}  = undef;
++    $synops{"slurmCormhapTaskLimit"}  = "Max corhmap tasks that can run at once";
++
 +    $global{"slurmArrayTaskLimit"}  = undef;
 +    $synops{"slurmArrayTaskLimit"}  = "Max tasks that can run at once";
 +
