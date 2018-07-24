fb9b74f4f1d037ae0c9c5a5c743f0c68fd3e1a3d
Jason Bacon <bacon4000@gmail.com>
2018-07-24 16:04:00 -0500

    xssp: Fix build, generate plist

--- xssp/files/patch-src_hssp-nt.cpp
+++ xssp/files/patch-src_hssp-nt.cpp
@@ -1,5 +1,14 @@
---- src/hssp-nt.cpp.orig	2018-07-24 16:46:11 UTC
+--- src/hssp-nt.cpp.orig	2018-04-06 08:17:20 UTC
 +++ src/hssp-nt.cpp
+@@ -23,7 +23,7 @@
+ #include <boost/lexical_cast.hpp>
+ #include <boost/range/adaptor/sliced.hpp>
+ #include <boost/regex.hpp>
+-#include <boost/tr1/cmath.hpp>
++#include <boost/units/cmath.hpp>
+ 
+ #include <algorithm>
+ #include <cmath>
 @@ -474,7 +474,7 @@ void MProfile::AdjustXGapCosts(std::vect
      {
        if (ix + d >= m_residues.size() or
--- xssp/files/patch-src_progress.h
+++ xssp/files/patch-src_progress.h
@@ -1,11 +1,16 @@
---- src/progress.h.orig	2018-07-24 16:57:56 UTC
+--- src/progress.h.orig	2018-04-06 08:17:20 UTC
 +++ src/progress.h
-@@ -6,7 +6,7 @@
+@@ -6,7 +6,12 @@
  
  #pragma once
  
 -#if defined(__linux__)
-+#if defined(__linux__) || defined(__FreeBSD__)
++#if defined(__FreeBSD__)
++
++#include <atomic>
++typedef std::atomic<int64>  MCounter;
++
++#elif defined(__linux__)
  
  #if defined(__INTEL_COMPILER_BUILD_DATE) || \
      (defined(__GNUC__) && (__GNUC__ > 4 || \
