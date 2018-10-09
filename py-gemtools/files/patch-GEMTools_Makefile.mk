1ae83bb847583145035a39c2136ef2c5c16fb43a
Jason Bacon <bacon4000@gmail.com>
2018-10-09 10:19:12 -0500

    py-gemtools: Successful build

--- py-gemtools/files/patch-GEMTools_Makefile.mk
+++ py-gemtools/files/patch-GEMTools_Makefile.mk
@@ -11,3 +11,12 @@
  
  # Folders
  FOLDER_BIN=$(ROOT_PATH)/bin
+@@ -29,7 +29,7 @@ HAVE_ZLIB = 1
+ HAVE_BZLIB = 1
+ HAVE_OPENMP = 1
+ 
+-GENERAL_FLAGS=-fPIC -Wall
++GENERAL_FLAGS=-fPIC -Wall -std=gnu89
+ ifeq ($(HAVE_ZLIB),1)
+ GENERAL_FLAGS:=$(GENERAL_FLAGS) -DHAVE_ZLIB
+ endif
