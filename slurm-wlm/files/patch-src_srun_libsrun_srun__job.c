38ab15e6352177caca0164c14c40a05073e1db87
Jason Bacon <bacon4000@gmail.com>
2018-12-01 10:23:14 -0600

    slurm-wlm: Restore proctrack default patch, enable build of switch/cray

--- slurm-wlm/files/patch-src_srun_libsrun_srun__job.c
+++ slurm-wlm/files/patch-src_srun_libsrun_srun__job.c
@@ -1,6 +1,6 @@
---- src/srun/libsrun/srun_job.c.orig	2018-09-30 05:41:23 UTC
+--- src/srun/libsrun/srun_job.c.orig	2018-10-24 20:21:46 UTC
 +++ src/srun/libsrun/srun_job.c
-@@ -105,6 +105,11 @@ static int shepherd_fd = -1;
+@@ -113,6 +113,11 @@ static int shepherd_fd = -1;
  static pthread_t signal_thread = (pthread_t) 0;
  static int pty_sigarray[] = { SIGWINCH, 0 };
  
