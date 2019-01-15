6d25724c1903b1c3c94fbe20daef4744e5695979
Jason Bacon <bacon4000@gmail.com>
2019-01-15 17:54:44 -0600

    epacts: Resolve conflict with texlive (epstopdf)

---
+++ epacts/files/patch-scripts_epacts.pm
@@ -0,0 +1,11 @@
+--- scripts/epacts.pm.orig	2019-01-15 23:46:52 UTC
++++ scripts/epacts.pm
+@@ -27,7 +27,7 @@ our $binawk = "awk";
+ our $binsort = "sort";
+ our $binpfbtops = "pfbtops";
+ our $bingnuplot = "gnuplot";
+-our $binepstopdf = "$epactsdir/bin/epstopdf";
++our $binepstopdf = "$epactsdir/bin/epacts-epstopdf";
+ our $xLabel = "X";
+ our $yLabel = "Y";
+ our $mtLabel = "MT";
