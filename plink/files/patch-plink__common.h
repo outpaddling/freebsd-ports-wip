42bdd67c02a87733d98928b31e4604948cce8893
Jason Bacon <bacon4000@gmail.com>
2019-02-28 08:39:05 -0600

    Fix for i386

--- plink/files/patch-plink__common.h
+++ plink/files/patch-plink__common.h
@@ -1,6 +1,6 @@
---- plink_common.h.orig	2017-08-27 17:35:52 UTC
+--- plink_common.h.orig	2019-02-27 08:24:47 UTC
 +++ plink_common.h
-@@ -116,8 +116,8 @@
+@@ -128,8 +128,8 @@
      // (may break on gcc <3.0?  but that shouldn't matter anymore)
      // tried defining GCC_VERSION, but that didn't always work
      #if (__GNUC__ <= 4) && (__GNUC_MINOR__ < 8)
@@ -11,3 +11,21 @@
      #endif
    #endif
  #endif
+@@ -141,17 +141,6 @@
+ #else
+   #define CTZLU __builtin_ctzl
+   #define CLZLU __builtin_clzl
+-  #ifndef __LP64__
+-    // attempt to patch GCC 6 build failure
+-    #if (__GNUC__ <= 4) && (__GNUC_MINOR__ < 8)
+-      #ifndef uintptr_t
+-        #define uintptr_t unsigned long
+-      #endif
+-      #ifndef intptr_t
+-        #define intptr_t long
+-      #endif
+-    #endif
+-  #endif
+ #endif
+ 
+ #ifdef __cplusplus
