a696b2f5c483d957b6486c878223de3e6bd1e21b
Charlie Root <root@outpaddling.cs.uwm.edu>
2019-04-21 13:53:42 -0500

    star: Upgrade to 2.7.0f

--- star/files/patch-source_SharedMemory.cpp
+++ star/files/patch-source_SharedMemory.cpp
@@ -1,5 +1,5 @@
---- source/SharedMemory.cpp.orig	2016-05-10 21:41:15 UTC
-+++ source/SharedMemory.cpp
+--- SharedMemory.cpp.orig	2016-05-10 21:41:15 UTC
++++ SharedMemory.cpp
 @@ -11,7 +11,7 @@
  #include <semaphore.h>
  #include <errno.h>
