2e45250dd91f0290eea6cbd8d38b3edfbb2f4505
Jason Bacon <bacon4000@gmail.com>
2019-05-12 08:38:16 -0500

    afni: Upgrade to 19.1.08

--- afni/files/patch-misc_math.c
+++ afni/files/patch-misc_math.c
@@ -1,6 +1,6 @@
---- misc_math.c.orig	2015-11-06 21:25:08 UTC
+--- misc_math.c.orig	2019-05-10 21:23:56 UTC
 +++ misc_math.c
-@@ -1,6 +1,13 @@
+@@ -1,5 +1,12 @@
  
 +/* For older libm */
 +double log2(double n)
@@ -9,8 +9,7 @@
 +}
 +
  /* move from Deconvolve.c into libmri.a         21 Jun 2010 [rickr] */
- 
 +
+ 
  double legendre( double x , int m )   /* Legendre polynomials over [-1,1] */
  {
-    if( m < 0 ) return 1.0 ;    /* bad input */
