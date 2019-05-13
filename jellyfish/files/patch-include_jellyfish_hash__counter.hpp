24cfb5ccaa4f929293ea02a1a2a73641b8568f4c
Jason Bacon <bacon4000@gmail.com>
2019-05-13 09:38:52 -0500

    jellyfish: Clean up, add htslib dep

--- jellyfish/files/patch-include_jellyfish_hash__counter.hpp
+++ jellyfish/files/patch-include_jellyfish_hash__counter.hpp
@@ -1,11 +1,11 @@
---- include/jellyfish/hash_counter.hpp.orig	2019-05-13 07:43:45 UTC
+--- include/jellyfish/hash_counter.hpp.orig	2018-04-16 12:46:09 UTC
 +++ include/jellyfish/hash_counter.hpp
-@@ -206,7 +206,7 @@ class hash_counter { (protected)
-       try {
-         new_ary_   = new array(ary_->size() * 2, ary_->key_len(), ary_->val_len(),
-                                ary_->max_reprobe(), ary_->reprobes());
--       } catch(typename array::ErrorAllocation e) {
-+       } catch(typename array::ErrorAllocation &e) {
+@@ -213,7 +213,7 @@ class hash_counter { (protected)
+           new_ary_ = new  array(ary_->size(), ary_->key_len(), ary_->val_len() + 1,
+                                 ary_->max_reprobe(), ary_->reprobes());
+         }
+-      } catch(typename array::ErrorAllocation e) {
++      } catch(typename array::ErrorAllocation &e) {
          new_ary_ = 0;
        }
      }
