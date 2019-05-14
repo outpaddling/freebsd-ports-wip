9c7cdee86edcbae2b34677972c4801f754092c26
Jason Bacon <bacon4000@gmail.com>
2019-05-14 11:48:27 -0500

    slurm-wlm: Move reinplaces to pre-configure to unbreak make makepatch

--- slurm-wlm/files/patch-src_plugins_switch_cray_switch__cray.c
+++ slurm-wlm/files/patch-src_plugins_switch_cray_switch__cray.c
@@ -1,4 +1,4 @@
---- src/plugins/switch/cray/switch_cray.c.orig	2018-10-24 20:21:46 UTC
+--- src/plugins/switch/cray/switch_cray.c.orig	2019-04-12 04:20:25 UTC
 +++ src/plugins/switch/cray/switch_cray.c
 @@ -35,7 +35,6 @@
   *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
@@ -18,7 +18,7 @@
  #include <math.h>
  #include <sched.h>
  #include <signal.h>
-@@ -1056,4 +1057,3 @@ extern int switch_p_job_step_post_resume
+@@ -1056,4 +1057,3 @@ extern int switch_p_job_step_post_resume(stepd_step_re
  #endif
  	return SLURM_SUCCESS;
  }
