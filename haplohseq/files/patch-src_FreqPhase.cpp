df967fb7fb45ef04eed45847bd72412eea36f292
Jason Bacon <bacon4000@gmail.com>
2019-11-15 18:29:22 -0600

    haplohseq: Error message for missing DP

--- haplohseq/files/patch-src_FreqPhase.cpp
+++ haplohseq/files/patch-src_FreqPhase.cpp
@@ -1,10 +1,20 @@
 --- src/FreqPhase.cpp.orig	2019-11-13 14:19:06 UTC
 +++ src/FreqPhase.cpp
-@@ -183,6 +183,7 @@ double FreqPhase::medianValue(const std:
+@@ -180,10 +180,16 @@ double FreqPhase::meanValue(const std::vector<double>&
+ double FreqPhase::medianValue(const std::vector<double>& values) {
+ 	double median;
+ 	size_t size = values.size();
++
++	if ( size == 0 ) {
++		std::cerr << "FreqPhase::medianValue(): values vector is empty." << endl;
++		std::cerr << "Make sure your VCF has all of GT:AD:DP." << endl;
++		exit(EX_DATAERR);
++	}
  	std::vector<double> tempFreqs(values);
  	sort(tempFreqs.begin(), tempFreqs.end());
  
-+	std::cerr << "size = " << size << '\n';
- 	if (size  % 2 == 0) {
+-	if (size  % 2 == 0) {
++	if (size % 2 == 0) {
  		median = (tempFreqs[size / 2 - 1] + tempFreqs[size / 2]) / 2;
  	}
+ 	else {
