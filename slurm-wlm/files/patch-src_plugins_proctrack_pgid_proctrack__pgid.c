150c9ac81c16e464c7a6650fcd19edd6dc020194
Jason Bacon <bacon4000@gmail.com>
2020-04-02 11:55:17 -0500

    slurm-wlm: Upgrade to 20.02.1

---
+++ slurm-wlm/files/patch-src_plugins_proctrack_pgid_proctrack__pgid.c
@@ -0,0 +1,73 @@
+--- src/plugins/proctrack/pgid/proctrack_pgid.c.orig	2020-04-02 14:21:47 UTC
++++ src/plugins/proctrack/pgid/proctrack_pgid.c
+@@ -49,6 +49,13 @@
+ #include <sys/types.h>
+ #include <unistd.h>
+ 
++#ifdef __FreeBSD__
++#include <sys/param.h>
++#include <sys/sysctl.h>
++#include <sys/user.h>
++#include <libprocstat.h>
++#endif
++
+ #include "slurm/slurm.h"
+ #include "slurm/slurm_errno.h"
+ #include "src/common/log.h"
+@@ -179,6 +186,10 @@ proctrack_p_wait(uint64_t cont_id)
+ 	return SLURM_SUCCESS;
+ }
+ 
++
++/*
++ * Get list of all PIDs belonging to process group cont_id
++ */
+ extern int
+ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, int *npids)
+ {
+@@ -193,8 +204,21 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
+ 	pid_t *pid_array = NULL;
+ 	int pid_count = 0;
+ 
+-	if ((dir = opendir("/proc")) == NULL) {
+-		error("opendir(/proc): %m");
++#ifdef __FreeBSD__
++	struct procstat		*procs; 
++	struct kinfo_proc	*proc_list;
++	size_t			c;
++
++	procs = procstat_open_sysctl();
++	proc_list = procstat_getprocs(procs, KERN_PROC_PGRP, 1392,
++					(unsigned *)&pid_count);
++	
++	xrealloc(pid_array, sizeof(pid_t) * pid_count);
++	for (c = 0; c < pid_count; ++c)
++		pid_array[c] = proc_list[c].ki_pid;
++#else
++	if ((dir = opendir("/compat/linux/proc")) == NULL) {
++		error("opendir(/compat/linux/proc): %m");
+ 		rc = SLURM_ERROR;
+ 		goto fini;
+ 	}
+@@ -209,11 +233,12 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
+ 			      num, ret_l);
+ 			continue;
+ 		}
+-		sprintf(path, "/proc/%s/stat", num);
++		sprintf(path, "/compat/linux/proc/%s/stat", num);
+ 		if ((fd = open(path, O_RDONLY)) < 0) {
+ 			continue;
+ 		}
+ 		buf_used = read(fd, rbuf, 4096);
++		// Just ignore failures to read process info??
+ 		if ((buf_used <= 0) || (buf_used >= 4096)) {
+ 			close(fd);
+ 			continue;
+@@ -236,6 +261,7 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
+ 	}
+ 	xfree(rbuf);
+ 	closedir(dir);
++#endif
+ 
+ fini:	*pids  = pid_array;
+ 	*npids = pid_count;
