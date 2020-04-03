b6b80c155d502f08c74b718fa93c11b1da894d98
Jason Bacon <bacon4000@gmail.com>
2020-04-03 10:43:28 -0500

    slurm-wlm: Silence procstat_getprocs() warning

--- slurm-wlm/files/patch-src_plugins_proctrack_pgid_proctrack__pgid.c
+++ slurm-wlm/files/patch-src_plugins_proctrack_pgid_proctrack__pgid.c
@@ -25,77 +25,117 @@
  extern int
  proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, int *npids)
  {
-@@ -193,32 +204,66 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
+@@ -193,6 +204,51 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
  	pid_t *pid_array = NULL;
  	int pid_count = 0;
  
--	if ((dir = opendir("/proc")) == NULL) {
--		error("opendir(/proc): %m");
++	// error("INFO: proctrack_p_get_pids() called.");
++
 +#ifdef __FreeBSD__
 +	struct procstat		*proc_info; 
 +	struct kinfo_proc	*proc_list;
 +	size_t			c;
++	FILE			*procstat_err;
 +
 +	proc_info = procstat_open_sysctl();
++
++	/* procstat_getprocs() prints an innocuous but annoying warning
++	   to stderr by default when no matching processes are found:
++
++		kinfo_proc structure size mismatch (len = 0)
++
++	   Silence by redirecting to /dev/null.
++	*/
++	if ( (procstat_err = fopen("/dev/null", "w+")) != NULL )
++		err_set_file(procstat_err);
 +	proc_list = procstat_getprocs(proc_info, KERN_PROC_PGRP, cont_id,
 +					(unsigned *)&pid_count);
++	if ( procstat_err != NULL )
++		fclose(procstat_err);
 +
 +	if ( pid_count > 0 )
 +	{
 +		xrealloc(pid_array, sizeof(pid_t) * pid_count);
++		// FIXME: Do we need to filter zombies like the Linux code?
++		// proc_list[c].ki_paddr->p_state == PRS_ZOMBIE
 +		for (c = 0; c < pid_count; ++c)
 +		{
-+			error("pid=%ld ppid=%ld pgid=%ld",
++			/* error("INFO: command=%s pid=%ld ppid=%ld pgid=%ld",
++				proc_list[c].ki_comm,
++				// srun hangs trying to access this
++				//proc_list[c].ki_paddr->p_state,
 +				proc_list[c].ki_pid, proc_list[c].ki_ppid,
-+				proc_list[c].ki_pgid);
++				proc_list[c].ki_pgid); */
 +			pid_array[c] = proc_list[c].ki_pid;
 +		}
 +	}
-+	else
-+		error("No PIDs found in group %lu.", cont_id);
 +
 +	procstat_freeprocs(proc_info, proc_list);
 +	procstat_close(proc_info);
 +#else
-+	// Is parsing files in /proc really the only way to get PIDs in Linux?
-+	if ((dir = opendir("/compat/linux/proc")) == NULL) {
-+		error("opendir(/compat/linux/proc): %m");
++	// Is parsing files in /proc the only way to collect PIDs on Linux?
+ 	if ((dir = opendir("/proc")) == NULL) {
+ 		error("opendir(/proc): %m");
  		rc = SLURM_ERROR;
- 		goto fini;
- 	}
+@@ -201,32 +257,56 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
  	rbuf = xmalloc(4096);
  	while ((de = readdir(dir)) != NULL) {
  		num = de->d_name;
-+		// Is this really sufficient to identify a procid directory?
++
++		// Is this sufficient to identify a PID directory?
++		// Should verify that *endptr == '\0'.
  		if ((num[0] < '0') || (num[0] > '9'))
  			continue;
  		ret_l = strtol(num, &endptr, 10);
-+		// Should check *endptr.  Should be '\0'.
-+		// Just ignore failures to read process info??
++
++		// Is this useful?  Existence of /proc/%s/stat
++		// is what really matters.
  		if ((ret_l == LONG_MIN) || (ret_l == LONG_MAX)) {
  			error("couldn't do a strtol on str %s(%ld): %m",
  			      num, ret_l);
  			continue;
  		}
--		sprintf(path, "/proc/%s/stat", num);
++
 +		// Use snprintf() here to protect against buffer overflow?
-+		sprintf(path, "/compat/linux/proc/%s/stat", num);
+ 		sprintf(path, "/proc/%s/stat", num);
  		if ((fd = open(path, O_RDONLY)) < 0) {
  			continue;
  		}
++
++		// Why not read straight from stream with fscanf()?
  		buf_used = read(fd, rbuf, 4096);
-+		// Why not just read straight from stream with fscanf()?
-+		// Just ignore failures to read process info??
++
++		// Just ignore failures to read process info?
++		// No error reporting?
  		if ((buf_used <= 0) || (buf_used >= 4096)) {
  			close(fd);
  			continue;
  		}
  		close(fd);
-+		// Just ignore failures to read process info??
++
++		// Just ignore failures to read process info?
++		// No error reporting?  If this fails after a successful
++		// fopen(), something is very wrong and it should probably
++		// be flagged to facilate debugging
  		if (sscanf(rbuf, "%ld %s %c %ld %ld",
  			   &pid, cmd, &state, &ppid, &pgid) != 5) {
  			continue;
-@@ -236,6 +281,7 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
+ 		}
++		
+ 		if (pgid != (long) cont_id)
+ 			continue;
++
++		/* error("INFO: command=%s state=%c "
++		       "pid=%ld ppid=%ld pgid=%ld",
++		       cmd, state, pid, ppid, pgid); */
++
+ 		if (state == 'Z') {
+-			debug3("Defunct process skipped: command=%s state=%c "
++			error("Defunct process skipped: command=%s state=%c "
+ 			       "pid=%ld ppid=%ld pgid=%ld",
+ 			       cmd, state, pid, ppid, pgid);
+ 			continue;	/* Defunct, don't try to kill */
+@@ -236,6 +316,7 @@ proctrack_p_get_pids(uint64_t cont_id, pid_t **pids, i
  	}
  	xfree(rbuf);
  	closedir(dir);
