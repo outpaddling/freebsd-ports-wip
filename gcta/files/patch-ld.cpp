7b8f71fee4c514c03e4d742927c3f339be9c9c16
Jason Bacon <bacon4000@gmail.com>
2020-04-15 08:09:43 -0500

    gcta: Clean up

--- gcta/files/patch-ld.cpp
+++ gcta/files/patch-ld.cpp
@@ -1,14 +1,13 @@
---- ld.cpp.orig	2020-04-15 01:52:59 UTC
+--- ld.cpp.orig	2020-04-15 12:56:27 UTC
 +++ ld.cpp
-@@ -1004,7 +1004,10 @@ void gcta::calcu_max_ld_rsq_blk(eigenVector &multi_rsq
+@@ -1004,7 +1004,9 @@ void gcta::calcu_max_ld_rsq_blk(eigenVector &multi_rsq
          }
  
  
 -        SelfAdjointEigenSolver<MatrixXf> pca(rsq_sub.array());
-+	// Fixed compile by removing (rsq_sub.array())
-+	// Apparently constructor doesn't come in a 1 arg version
-+	// What's the equivalent?
-+        SelfAdjointEigenSolver<MatrixXf> pca; // (rsq_sub.array());
++	// Fixed compile by removing .array().  Not sure about the validity
++	// of this change, but it seemed reasonable based on constructor docs.
++        SelfAdjointEigenSolver<MatrixXf> pca(rsq_sub);
  
                  // debug
         // ofstream tmp("tmp_R.txt");
