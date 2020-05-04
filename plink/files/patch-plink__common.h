228757c788c2025368dc1dbf45a48c21e9dc8e8d
Jason Bacon <bacon4000@gmail.com>
2020-05-04 10:57:07 -0500

    plink: Update to mzaki version

--- plink/files/patch-plink__common.h
+++ plink/files/patch-plink__common.h
@@ -1,31 +1,20 @@
---- plink_common.h.orig	2019-02-27 08:24:47 UTC
+--- plink_common.h.orig	2020-01-22 01:51:56 UTC
 +++ plink_common.h
-@@ -128,8 +128,8 @@
+@@ -122,7 +122,7 @@
+   #define EOLN_STR "\n"
+   #define FOPEN_RB "r"
+   #define FOPEN_WB "w"
+-  #ifndef __APPLE__
++  #if !defined(__APPLE__) && !defined(__FreeBSD__)
+     // argh
+     // not sure what the right threshold actually is, but this works for now
      // (may break on gcc <3.0?  but that shouldn't matter anymore)
-     // tried defining GCC_VERSION, but that didn't always work
-     #if (__GNUC__ <= 4) && (__GNUC_MINOR__ < 8)
--      #define uint64_t unsigned long long
--      #define int64_t long long
-+      //#define uint64_t unsigned long long
-+      //#define int64_t long long
-     #endif
-   #endif
- #endif
-@@ -141,17 +141,6 @@
- #else
-   #define CTZLU __builtin_ctzl
+@@ -143,7 +143,7 @@
    #define CLZLU __builtin_clzl
--  #ifndef __LP64__
--    // attempt to patch GCC 6 build failure
+   #ifndef __LP64__
+     // attempt to patch GCC 6 build failure
 -    #if (__GNUC__ <= 4) && (__GNUC_MINOR__ < 8)
--      #ifndef uintptr_t
--        #define uintptr_t unsigned long
--      #endif
--      #ifndef intptr_t
--        #define intptr_t long
--      #endif
--    #endif
--  #endif
- #endif
- 
- #ifdef __cplusplus
++    #if !defined(__FreeBSD__) && (__GNUC__ <= 4) && (__GNUC_MINOR__ < 8)
+       #ifndef uintptr_t
+         #define uintptr_t unsigned long
+       #endif
