7d0f6509f351bdf5e08c2cb6510498cac75aadb9
Jason Bacon <bacon4000@gmail.com>
2020-07-13 14:47:46 -0500

    htslib: Add debug output for curl reference download errors

--- htslib/files/patch-hfile__libcurl.c
+++ htslib/files/patch-hfile__libcurl.c
@@ -1,6 +1,6 @@
---- hfile_libcurl.c.orig	2017-09-28 11:20:23 UTC
+--- hfile_libcurl.c.orig	2020-07-08 12:52:24 UTC
 +++ hfile_libcurl.c
-@@ -49,6 +49,10 @@ typedef struct {
+@@ -74,6 +74,10 @@ typedef struct {
      unsigned int size;
  } hdrlist;
  
@@ -11,3 +11,30 @@
  typedef struct {
      hdrlist fixed;                   // List of headers supplied at hopen()
      hdrlist extra;                   // List of headers from callback
+@@ -126,6 +130,8 @@ static int restart_from_position(hFILE_libcurl *fp, of
+ 
+ static int http_status_errno(int status)
+ {
++    fprintf(stderr, "http_status_errno: status = %d\n", status);
++
+     if (status >= 500)
+         switch (status) {
+         case 501: return ENOSYS;
+@@ -151,6 +157,8 @@ static int easy_errno(CURL *easy, CURLcode err)
+ {
+     long lval;
+ 
++    fprintf(stderr, "easy_errno: err = %d\n", err);
++
+     switch (err) {
+     case CURLE_OK:
+         return 0;
+@@ -227,6 +235,8 @@ static int easy_errno(CURL *easy, CURLcode err)
+ 
+ static int multi_errno(CURLMcode errm)
+ {
++    fprintf(stderr, "multi_errno: errm = %d\n", errm);
++
+     switch (errm) {
+     case CURLM_CALL_MULTI_PERFORM:
+     case CURLM_OK:
