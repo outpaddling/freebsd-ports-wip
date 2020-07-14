89ed4a771950562d81c12146ebc535d0f78e1b75
Jason Bacon <bacon4000@gmail.com>
2020-07-14 08:21:32 -0500

    htslib: Proposed fix for latest libcurl

--- htslib/files/patch-hfile__libcurl.c
+++ htslib/files/patch-hfile__libcurl.c
@@ -29,7 +29,15 @@
      switch (err) {
      case CURLE_OK:
          return 0;
-@@ -227,6 +235,8 @@ static int easy_errno(CURL *easy, CURLcode err)
+@@ -221,12 +229,16 @@ static int easy_errno(CURL *easy, CURLcode err)
+         return EEXIST;
+ 
+     default:
++        hts_log_error("Libcurl reported error %d (%s)", (int) err,
++                      curl_easy_strerror(err));
+         return EIO;
+     }
+ }
  
  static int multi_errno(CURLMcode errm)
  {
@@ -38,3 +46,55 @@
      switch (errm) {
      case CURLM_CALL_MULTI_PERFORM:
      case CURLM_OK:
+@@ -241,6 +253,8 @@ static int multi_errno(CURLMcode errm)
+         return ENOMEM;
+ 
+     default:
++        hts_log_error("Libcurl reported error %d (%s)", (int) errm,
++                      curl_multi_strerror(errm));
+         return EIO;
+     }
+ }
+@@ -818,9 +832,13 @@ static ssize_t libcurl_read(hFILE *fpv, void *bufferv,
+         fp->buffer.ptr.rd = buffer;
+         fp->buffer.len = nbytes;
+         fp->paused = 0;
+-        err = curl_easy_pause(fp->easy, CURLPAUSE_CONT);
+-        if (err != CURLE_OK) { errno = easy_errno(fp->easy, err); return -1; }
+-
++	if (! fp->finished) {
++            err = curl_easy_pause(fp->easy, CURLPAUSE_CONT);
++            if (err != CURLE_OK) {
++		errno = easy_errno(fp->easy, err);
++		return -1;
++	    }
++	}
+         while (! fp->paused && ! fp->finished) {
+             if (wait_perform(fp) < 0) return -1;
+         }
+@@ -1046,12 +1064,6 @@ static int restart_from_position(hFILE_libcurl *fp, of
+     }
+     temp_fp.nrunning = ++fp->nrunning;
+ 
+-    err = curl_easy_pause(temp_fp.easy, CURLPAUSE_CONT);
+-    if (err != CURLE_OK) {
+-        save_errno = easy_errno(temp_fp.easy, err);
+-        goto error_remove;
+-    }
+-
+     while (! temp_fp.paused && ! temp_fp.finished)
+         if (wait_perform(&temp_fp) < 0) {
+             save_errno = errno;
+@@ -1127,8 +1139,10 @@ static int libcurl_close(hFILE *fpv)
+     fp->buffer.len = 0;
+     fp->closing = 1;
+     fp->paused = 0;
+-    err = curl_easy_pause(fp->easy, CURLPAUSE_CONT);
+-    if (err != CURLE_OK) save_errno = easy_errno(fp->easy, err);
++    if (! fp->finished) {
++        err = curl_easy_pause(fp->easy, CURLPAUSE_CONT);
++        if (err != CURLE_OK) save_errno = easy_errno(fp->easy, err);
++    }
+ 
+     while (save_errno == 0 && ! fp->paused && ! fp->finished)
+         if (wait_perform(fp) < 0) save_errno = errno;
