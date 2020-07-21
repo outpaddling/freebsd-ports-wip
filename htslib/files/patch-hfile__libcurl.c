0c134c4d081396fe25e5857bfed67a7a89e7c979
Jason Bacon <bacon4000@gmail.com>
2020-07-21 14:48:53 -0500

    htslib: Remove temporary debug code

--- htslib/files/patch-hfile__libcurl.c
+++ htslib/files/patch-hfile__libcurl.c
@@ -1,4 +1,4 @@
---- hfile_libcurl.c.orig	2020-07-08 12:52:24 UTC
+--- hfile_libcurl.c.orig	2019-12-19 09:54:30 UTC
 +++ hfile_libcurl.c
 @@ -74,6 +74,10 @@ typedef struct {
      unsigned int size;
@@ -11,25 +11,7 @@
  typedef struct {
      hdrlist fixed;                   // List of headers supplied at hopen()
      hdrlist extra;                   // List of headers from callback
-@@ -126,6 +130,8 @@ static int restart_from_position(hFILE_libcurl *fp, of
- 
- static int http_status_errno(int status)
- {
-+    fprintf(stderr, "http_status_errno: status = %d\n", status);
-+
-     if (status >= 500)
-         switch (status) {
-         case 501: return ENOSYS;
-@@ -151,6 +157,8 @@ static int easy_errno(CURL *easy, CURLcode err)
- {
-     long lval;
- 
-+    fprintf(stderr, "easy_errno: err = %d\n", err);
-+
-     switch (err) {
-     case CURLE_OK:
-         return 0;
-@@ -221,12 +229,16 @@ static int easy_errno(CURL *easy, CURLcode err)
+@@ -218,6 +222,8 @@ static int easy_errno(CURL *easy, CURLcode err)
          return EEXIST;
  
      default:
@@ -38,15 +20,7 @@
          return EIO;
      }
  }
- 
- static int multi_errno(CURLMcode errm)
- {
-+    fprintf(stderr, "multi_errno: errm = %d\n", errm);
-+
-     switch (errm) {
-     case CURLM_CALL_MULTI_PERFORM:
-     case CURLM_OK:
-@@ -241,6 +253,8 @@ static int multi_errno(CURLMcode errm)
+@@ -238,6 +244,8 @@ static int multi_errno(CURLMcode errm)
          return ENOMEM;
  
      default:
@@ -55,7 +29,7 @@
          return EIO;
      }
  }
-@@ -818,9 +832,13 @@ static ssize_t libcurl_read(hFILE *fpv, void *bufferv,
+@@ -797,9 +805,13 @@ static ssize_t libcurl_read(hFILE *fpv, void *bufferv,
          fp->buffer.ptr.rd = buffer;
          fp->buffer.len = nbytes;
          fp->paused = 0;
@@ -72,7 +46,7 @@
          while (! fp->paused && ! fp->finished) {
              if (wait_perform(fp) < 0) return -1;
          }
-@@ -1046,12 +1064,6 @@ static int restart_from_position(hFILE_libcurl *fp, of
+@@ -1003,12 +1015,6 @@ static int restart_from_position(hFILE_libcurl *fp, of
      }
      temp_fp.nrunning = ++fp->nrunning;
  
@@ -85,7 +59,7 @@
      while (! temp_fp.paused && ! temp_fp.finished)
          if (wait_perform(&temp_fp) < 0) {
              save_errno = errno;
-@@ -1127,8 +1139,10 @@ static int libcurl_close(hFILE *fpv)
+@@ -1084,8 +1090,10 @@ static int libcurl_close(hFILE *fpv)
      fp->buffer.len = 0;
      fp->closing = 1;
      fp->paused = 0;
