29098ab0ec1b73ce5c78cec4c8697b658701cf55
Jason Bacon <bacon4000@gmail.com>
2020-12-07 16:49:30 -0600

    slurm-wlm: Fix -fno-common patch that removed some important defs

--- slurm-wlm/files/patch-src_plugins_select_cons__tres_job__test.c
+++ slurm-wlm/files/patch-src_plugins_select_cons__tres_job__test.c
@@ -1,26 +1,17 @@
---- src/plugins/select/cons_tres/job_test.c.orig	2020-03-26 21:44:05 UTC
+--- src/plugins/select/cons_tres/job_test.c.orig	2020-12-07 22:40:47 UTC
 +++ src/plugins/select/cons_tres/job_test.c
-@@ -41,23 +41,6 @@
- 
- #define _DEBUG 0	/* Enables module specific debugging */
- 
--/*
-- * These symbols are defined here so when we link with something other
-- * than the slurmctld we will have these symbols defined. They will get
-- * overwritten when linking with the slurmctld.
-- */
--#if defined (__APPLE__)
--extern slurmctld_config_t slurmctld_config __attribute__((weak_import));
--extern bitstr_t *idle_node_bitmap __attribute__((weak_import));
--extern node_record_t *node_record_table_ptr __attribute__((weak_import));
--extern List job_list __attribute__((weak_import));
--#else
+@@ -52,10 +52,10 @@ extern bitstr_t *idle_node_bitmap __attribute__((weak_
+ extern node_record_t *node_record_table_ptr __attribute__((weak_import));
+ extern List job_list __attribute__((weak_import));
+ #else
 -slurmctld_config_t slurmctld_config;
 -bitstr_t *idle_node_bitmap;
 -node_record_t *node_record_table_ptr;
 -List job_list;
--#endif
--
++extern slurmctld_config_t slurmctld_config;
++extern bitstr_t *idle_node_bitmap;
++extern node_record_t *node_record_table_ptr;
++extern List job_list;
+ #endif
+ 
  typedef struct node_weight_struct {
- 	bitstr_t *node_bitmap;	/* bitmap of nodes with this weight */
- 	uint32_t weight;	/* priority of node for scheduling work on */
