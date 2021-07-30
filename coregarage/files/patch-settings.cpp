8cf7473443fe164d4da7e47ad066616158045103
Jason Bacon <jwb@FreeBSD.org>
2021-07-30 08:59:48 -0500

    coregarage:
    Patch hard-coded /usr/bin, etc.
    Default to user shell from getpwuid()

---
+++ coregarage/files/patch-settings.cpp
@@ -0,0 +1,32 @@
+--- settings.cpp.orig	2021-04-11 08:34:27 UTC
++++ settings.cpp
+@@ -34,6 +34,10 @@
+ #include <cprime/themefunc.h>
+ #include <cprime/filefunc.h>
+ 
++#include <sys/types.h>	// getpwuid()
++#include <pwd.h>	// getpwuid()
++#include <unistd.h>	// getuid()
++
+ #include "settings.h"
+ 
+ settings::settings()
+@@ -60,6 +64,8 @@ settings::~settings()
+ 
+ void settings::setDefaultSettings()
+ {
++    struct passwd *pwent;
++
+     // Save screenshot directory
+     if (!cSetting->value("CoreShot/SaveLocation").toString().count()) {
+         QDir().mkpath(QStandardPaths::writableLocation(QStandardPaths::PicturesLocation) + "/Screen Shots");
+@@ -104,7 +110,8 @@ void settings::setDefaultSettings()
+     cSetting->setValue("CoreKeyboard/Font", genFont.family());
+ 
+     cSetting->setValue("CoreTerminal/Font", monoFont);
+-    cSetting->setValue("CoreTerminal/Shell", "/usr/bin/bash");
++    pwent = getpwuid(getuid());
++    cSetting->setValue("CoreTerminal/Shell", pwent->pw_shell);
+     cSetting->setValue("CoreTerminal/Opacity", 70);
+     cSetting->setValue("CoreTerminal/HistorySize", 500);
+     cSetting->setValue("CoreTerminal/KeyTab", "linux");
