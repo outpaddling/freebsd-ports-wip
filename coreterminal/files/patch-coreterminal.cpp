0dc36823cd71e929582c66ca10ff392740fc6e20
Jason Bacon <jwb@FreeBSD.org>
2021-08-03 09:11:10 -0500

    coreterminal: May have found correct tabBar height

--- coreterminal/files/patch-coreterminal.cpp
+++ coreterminal/files/patch-coreterminal.cpp
@@ -17,34 +17,40 @@
  
  #include <cprime/appopenfunc.h>
  #include <cprime/variables.h>
-@@ -267,7 +270,30 @@ void CoreTerminal::setWindowProperties()
+@@ -267,7 +270,36 @@ void CoreTerminal::setWindowProperties()
          setStyleSheet( "#base { background-color: palette(Window); }" );
      }
  
 -    resize( 800, 500 );
 +    QFontMetrics fm( terminalFont );
-+    std::cerr << "Font em width = " << fm.boundingRect( "M" ).width() 
-+		<< "\nFont em height = " << fm.boundingRect("M").height() 
-+		<< "\nFont avg width = " << fm.averageCharWidth()
-+		<< "\nFont height = " << fm.height()
-+		<< "\nTab bar width = " << TabWidget->tabBar()->width()
-+		<< "\nTab bar height = " << TabWidget->tabBar()->height()
-+		<< "\nScroll bar width = " << qApp->style()->pixelMetric(QStyle::PM_ScrollBarExtent) 
-+		//<< "\nframeGeom = " << frameGeometry().height()
-+		//<< "\ngeom = " << geometry().height()
-+		<< "\nTabBarTabOverlap = " << qApp->style()->pixelMetric(QStyle::PM_TabBarTabOverlap)
-+		<< "\nTabBarTabVSpace = " << qApp->style()->pixelMetric(QStyle::PM_TabBarTabVSpace)
-+		<< "\nTabBarBaseHeight = " << qApp->style()->pixelMetric(QStyle::PM_TabBarBaseHeight)
-+		<< "\nTabBarBaseOverlap = " << qApp->style()->pixelMetric(QStyle::PM_TabBarBaseOverlap)
-+		<< "\nTabBar->height() = " << TabWidget->tabBar()->height()
-+		<< "\nTabBar->geometry().height() = " << TabWidget->tabBar()->geometry().height()
++    std::cerr	<< "fm.boundingRect(\"M\").width() = " << fm.boundingRect( "M" ).width() 
++		<< "\nfm.boundingRect(\"M\").height() = " << fm.boundingRect("M").height() 
++		<< "\nfm.averageCharWidth() = " << fm.averageCharWidth()
++		<< "\nfm.height() = " << fm.height()
++		<< "\ntabBar.width() = " << TabWidget->tabBar()->width()
++		<< "\ntabBar.height() = " << TabWidget->tabBar()->height()
++		<< "\nframeGeometry.height() = " << frameGeometry().height()
++		<< "\ngeometry.height() = " << geometry().height()
++		<< "\nPM_ScrollBarExtent = " << qApp->style()->pixelMetric(QStyle::PM_ScrollBarExtent) 
++		<< "\nPM_TabBarTabOverlap = " << qApp->style()->pixelMetric(QStyle::PM_TabBarTabOverlap)
++		<< "\nPM_TabBarTabVSpace = " << qApp->style()->pixelMetric(QStyle::PM_TabBarTabVSpace)
++		<< "\nPM_TabBarBaseHeight = " << qApp->style()->pixelMetric(QStyle::PM_TabBarBaseHeight)
++		<< "\nPM_TabBarBaseOverlap = " << qApp->style()->pixelMetric(QStyle::PM_TabBarBaseOverlap)
++		<< "\ntabBar()->height() = " << TabWidget->tabBar()->height()
++		<< "\ntabBar()->tabRect().height() = " << TabWidget->tabBar()->tabRect(0).height()
 +		<< '\n';
 +    int width = fm.averageCharWidth() * 80 + 2
 +		+ qApp->style()->pixelMetric(QStyle::PM_ScrollBarExtent);
-+    // +8 for Fusion, +4 for QtCurve, +3 for Windows
++    /*
++     * Perfect size =
++     * tabBar->height() +7 for Fusion, +4 for QtCurve, +3 for Windows
++     * tabBar->tabRect(0).height() gets very close:
++     * 36 for Fusion, 33 for QtCurve, 32 for Windows
++     * Needs to be verified for other themes
++     */
 +    int height = fm.height() * 30
-+		+ TabWidget->tabBar()->height()
-+		+ 8;
++		+ TabWidget->tabBar()->tabRect(0).height()
++		+ 1;
 +    resize(width, height);
  
      if (uiMode != 0) {
