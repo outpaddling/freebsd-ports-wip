cc18cbb2960386bb18689b1f319ee59d1efad9c7
Jason Bacon <jwb@FreeBSD.org>
2021-09-21 10:47:24 -0500

    slurm-wlm-devel: Augment debug code

--- slurm-wlm-devel/files/patch-src_common_slurm__protocol__api.c
+++ slurm-wlm-devel/files/patch-src_common_slurm__protocol__api.c
@@ -8,7 +8,29 @@
  	memset(sin, 0, sizeof(*sin));
  
  	if (slurm_addr_is_unspec(&s_addr)) {
-@@ -2896,6 +2897,7 @@ extern void slurm_setup_addr(slurm_addr_t *sin, uint16
+@@ -2880,7 +2881,7 @@ extern void slurm_setup_addr(slurm_addr_t *sin, uint16
+ 		else
+ 			var = "NoInAddrAny";
+ 
+-		if (xstrcasestr(slurm_conf.comm_params, var)) {
++		if ( xstrcasestr(slurm_conf.comm_params, var)) {
+ 			char host[MAXHOSTNAMELEN];
+ 
+ 			if (!gethostname(host, MAXHOSTNAMELEN)) {
+@@ -2889,13 +2890,20 @@ extern void slurm_setup_addr(slurm_addr_t *sin, uint16
+ 				fatal("%s: Can't get hostname or addr: %m",
+ 				      __func__);
+ 		} else {
++			fprintf(stderr, "Using NULL host for slurm_set_addr().\n");
+ 			slurm_set_addr(&s_addr, port, NULL);
+ 		}
+ 	}
+ 
++	fprintf(stderr, "Back from slurm_set_addr\n");
++	for (int c = 0; c < 14; ++c)
++	    fprintf(stderr, "%u ",
++		(unsigned char)s_addr.__ss_pad1[c]);
++	putc('\n', stderr);
  	memcpy(sin, &s_addr, sizeof(*sin));
  	slurm_set_port(sin, port);
  	log_flag(NET, "%s: update address to %pA", __func__, sin);
--- slurm-wlm-devel/files/patch-src_common_slurm__protocol__socket.c
+++ slurm-wlm-devel/files/patch-src_common_slurm__protocol__socket.c
@@ -10,18 +10,23 @@
  	for (retry=0; retry < PORT_RETRIES ; retry++) {
  		rc = bind(sockfd, (struct sockaddr *) &sin, sizeof(sin));
  		if (rc >= 0)
-@@ -422,6 +425,10 @@ extern int slurm_init_msg_engine(slurm_addr_t *addr)
+@@ -422,6 +425,15 @@ extern int slurm_init_msg_engine(slurm_addr_t *addr)
  		goto error;
  	}
  
++	fprintf(stderr, "slurm_init_msg_engine()...\n");
 +	fprintf(stderr, "fd = %d  addr = %p\n", fd, addr);
 +	fprintf(stderr, "ss_len = %u  ss_family = %u  __ss_align = %lu\n",
 +		addr->ss_len, addr->ss_family, addr->__ss_align);
 +	fprintf(stderr, "sizeof(*addr) = %zu\n", sizeof(*addr));
++	// fprintf(stderr, "port = %u\n", (unsigned short)(addr->__ss_pad1[1] + (addr->__ss_pad1[0] << 8)));
++	for (int c = 0; c < 14; ++c)
++	    fprintf(stderr, "%u ", (unsigned char)addr->__ss_pad1[c]);
++	putc('\n', stderr);
  	rc = bind(fd, (struct sockaddr const *) addr, sizeof(*addr));
  	if (rc < 0) {
  		error("Error binding slurm stream socket: %m");
-@@ -669,6 +676,7 @@ extern void slurm_set_addr(slurm_addr_t *addr, uint16_
+@@ -669,6 +681,7 @@ extern void slurm_set_addr(slurm_addr_t *addr, uint16_
  	memcpy(addr, ai_ptr->ai_addr, ai_ptr->ai_addrlen);
  	log_flag(NET, "%s: update addr. addr='%pA'", __func__, addr);
  	freeaddrinfo(ai_start);
