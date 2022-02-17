32b2dc847f9a65aac495e5bb7243999f42ee580a
Jason Bacon <jwb@FreeBSD.org>
2022-02-16 19:27:41 -0600

    Use proper sysctl() calls for battery status

--- lumina-core/files/patch-libLumina_LuminaOS-FreeBSD.cpp
+++ lumina-core/files/patch-libLumina_LuminaOS-FreeBSD.cpp
@@ -1,11 +1,65 @@
---- libLumina/LuminaOS-FreeBSD.cpp.orig	2022-02-16 22:46:44 UTC
+--- libLumina/LuminaOS-FreeBSD.cpp.orig	2021-12-26 02:33:45 UTC
 +++ libLumina/LuminaOS-FreeBSD.cpp
-@@ -301,7 +301,7 @@ bool LOS::hasBattery(){
+@@ -9,6 +9,7 @@
+ #include <unistd.h>
+ #include <sys/types.h>
+ #include <sys/sysctl.h>
++#include <dev/acpica/acpiio.h>
+ 
+ #include <QDebug>
+ //can't read xbrightness settings - assume invalid until set
+@@ -291,29 +292,46 @@ void LOS::systemSuspend(){
+ //Battery Availability
+ bool LOS::hasBattery(){
+   static int hasbat = -1;
++  int        life;
++  size_t     len = sizeof(life);
+   if(hasbat < 0 ){
+-    int val = batteryCharge();
+-    if(val >= 0 && val <= 100){ hasbat = 1; }
+-    else{ hasbat = 0; }
++    if ( sysctlbyname("hw.acpi.battery.life", &life, &len, NULL, 0) == 0 )
++      hasbat = 1;
++    else
++      hasbat = 0;
+   }
+   return (hasbat==1);
+ }
  
  //Battery Charge Level
  int LOS::batteryCharge(){ //Returns: percent charge (0-100), anything outside that range is counted as an error
 -  int charge = LUtils::getCmdOutput("apm -l").join("").toInt();
-+  int charge = LUtils::getCmdOutput("sysctl -n hw.acpi.battery.life").join("").toInt();
-   if(charge > 100){ charge = -1; } //invalid charge
-   return charge;
+-  if(charge > 100){ charge = -1; } //invalid charge
+-  return charge;
++  int    life;	// sysctl name
++  size_t len = sizeof(life);
++  if ( (sysctlbyname("hw.acpi.battery.life", &life, &len, NULL, 0) != 0) ||
++       (life > 100) )
++     life = -1; //invalid charge
++  return life;
  }
+ 
+ //Battery Charging State
+ bool LOS::batteryIsCharging(){
+-  return (LUtils::getCmdOutput("apm -a").join("").simplified() == "1");
++  int	 state;
++  size_t len = sizeof(state);
++  if ( (sysctlbyname("hw.acpi.battery.state", &state, &len, NULL, 0) == 0) &&
++       (state == ACPI_BATT_STAT_CHARGING) )
++    return true;
++  else
++    return false;
+ }
+ 
+ //Battery Time Remaining
+ int LOS::batterySecondsLeft(){ //Returns: estimated number of seconds remaining
+-  return LUtils::getCmdOutput("apm -t").join("").toInt();
++  int	 min;
++  size_t len = sizeof(min);
++  if ( sysctlbyname("hw.acpi.battery.min", &min, &len, NULL, 0) == 0 )
++    return min;
++  else
++    return -1;
+ }
+ 
+ //File Checksums
