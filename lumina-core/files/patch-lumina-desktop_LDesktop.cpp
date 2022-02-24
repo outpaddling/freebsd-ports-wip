35016d54952111247a8eae0bb5e2bc78b25c30a1
Jason Bacon <jwb@FreeBSD.org>
2022-02-23 20:48:10 -0600

    lumina-core: Experimental patches to update background on resize

---
+++ lumina-core/files/patch-lumina-desktop_LDesktop.cpp
@@ -0,0 +1,22 @@
+--- lumina-desktop/LDesktop.cpp.orig	2021-12-26 02:33:45 UTC
++++ lumina-desktop/LDesktop.cpp
+@@ -208,7 +208,7 @@ void LDesktop::InitDesktop(){
+   //This is called *once* during the main initialization routines
+   checkResolution(); //Adjust the desktop config file first (if necessary)
+   if(DEBUG){ qDebug() << "Init Desktop:" << Screen(); }
+-    //connect(desktop, SIGNAL(resized(int)), this, SLOT(UpdateGeometry(int)));
++    //connect(bgDesktop, SIGNAL(resized(int)), this, SLOT(UpdateGeometry(int)));
+   if(DEBUG){ qDebug() << "Desktop #"<<Screen()<<" -> "<< QGuiApplication::screens().at(Screen())->availableGeometry() << LSession::handle()->screenGeom(Screen()); }
+   deskMenu = new QMenu(0);
+     connect(deskMenu, SIGNAL(triggered(QAction*)), this, SLOT(SystemApplication(QAction*)) );
+@@ -223,6 +223,10 @@ void LDesktop::InitDesktop(){
+   bgtimer = new QTimer(this);
+     bgtimer->setSingleShot(true);
+     connect(bgtimer, SIGNAL(timeout()), this, SLOT(UpdateBackground()) );
++    // in case the screen resolution changes
++    // Maybe use bgDesktop like above?
++    screen = LSession::desktop();
++    connect(screen, SIGNAL(resized(int)), this, SLOT(UpdateBackground()) );
+ 
+     connect(QApplication::instance(), SIGNAL(DesktopConfigChanged()), this, SLOT(SettingsChanged()) );
+     connect(QApplication::instance(), SIGNAL(DesktopFilesChanged()), this, SLOT(UpdateDesktop()) );
