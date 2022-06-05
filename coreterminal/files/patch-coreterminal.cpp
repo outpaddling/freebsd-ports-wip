badad39381a092644b75bcd46065735979dc227c
Jason Bacon <jwb@FreeBSD.org>
2022-06-05 18:04:51 -0500

    coreterminal: Update to latest commit
    Adds's snap to multiple of font size when resizing window

--- coreterminal/files/patch-coreterminal.cpp
+++ coreterminal/files/patch-coreterminal.cpp
@@ -1,11 +1,20 @@
---- coreterminal.cpp.orig	2022-06-03 10:02:31 UTC
+--- coreterminal.cpp.orig	2022-06-05 22:56:33 UTC
 +++ coreterminal.cpp
-@@ -254,7 +254,7 @@ void CoreTerminal::setWindowProperties()
- //    }
+@@ -55,7 +55,7 @@ CoreTerminal::CoreTerminal(settings *s, QWidget *paren
+ 	: QMainWindow(parent)
+ 	, uiMode(0)
+ 	, mRows(30)
+-	, mCols(120)
++	, mCols(80)
+ 	, smi(s)
+ {
+ 	createGUI();
+@@ -86,7 +86,7 @@ void CoreTerminal::loadSettings()
+ {
+ 	uiMode = smi->getValue("CoreApps", "UIMode");
+ 	mRows = smi->getValue("CoreTerminal", "Rows", 30);
+-	mCols = smi->getValue("CoreTerminal", "Columns", 120);
++	mCols = smi->getValue("CoreTerminal", "Columns", 80);
  
- 	int rows = smi->getValue("CoreTerminal", "Rows", 30);
--	int cols = smi->getValue("CoreTerminal", "Columns", 120);
-+	int cols = smi->getValue("CoreTerminal", "Columns", 80);
+ 	QFont terminalFont = smi->getValue("CoreTerminal", "Font");
  
- 	auto screenSize = QApplication::primaryScreen()->availableSize();
- 	int w = qMin(screenSize.width(), getWidthByColumn(cols));
