5cef6174a7da9bea971627284ad0c15e2d26f567
Jason Bacon <jwb@FreeBSD.org>
2022-10-16 07:54:09 -0500

    py-multiqc: Update to 1.13

--- py-multiqc/files/patch-multiqc_multiqc.py
+++ py-multiqc/files/patch-multiqc_multiqc.py
@@ -1,28 +1,18 @@
---- multiqc/multiqc.py.orig	2021-07-05 07:13:52 UTC
+--- multiqc/multiqc.py.orig	2022-09-08 21:36:30 UTC
 +++ multiqc/multiqc.py
-@@ -11,17 +11,26 @@ from __future__ import print_function
- 
- import base64
- import click
-+import os
+@@ -18,10 +18,16 @@ import jinja2
+ import os
+ import re
+ import rich
+-import rich_click as click
 +import sys
-+
 +if 'LC_ALL' in os.environ and 'LANG' in os.environ:
-+    import click
++    import rich_click as click
 +else:
 +    print('multiqc: LC_ALL and LANG must be set to a UTF-8 character set')
 +    print('in your environment in order for the click module to function.')
 +    print('E.g. export LC_ALL=en_US.UTF-8 or setenv LC_ALL en_US.UTF-8')
 +    sys.exit()
-+
- from distutils import version
- from distutils.dir_util import copy_tree
- import errno
- import io
- import jinja2
--import os
- import re
- import rich
  import shutil
  import subprocess
 -import sys
