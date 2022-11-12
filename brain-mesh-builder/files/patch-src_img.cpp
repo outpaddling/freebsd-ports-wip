5fea7b301dfb0f41ce6f0fb830ff1bf954b217e5
Jason Bacon <jwb@FreeBSD.org>
2022-11-12 11:31:31 -0600

    brain-mesh-builder: Update to 1.1

--- brain-mesh-builder/files/patch-src_img.cpp
+++ brain-mesh-builder/files/patch-src_img.cpp
@@ -1,4 +1,4 @@
---- src/img.cpp.orig	2021-04-15 21:30:36 UTC
+--- src/img.cpp.orig	2022-11-12 14:21:36 UTC
 +++ src/img.cpp
 @@ -1109,7 +1109,7 @@ void img::thresholdAuto(double cutoff, int nbin, strin
    unsigned char **pxl_tmp=new unsigned char*[rows];
@@ -27,3 +27,31 @@
    int i,j,k;
    
    svec[0]=vimg.begin()->tag; svec[1]=vimg.rbegin()->tag; // default
+@@ -1497,7 +1497,7 @@ void mergeImg(img *img0, img *img1, string mode,string
+      If tag2 given create new img, img0 and img1 will be unmodified. */
+   
+   int i,j,k=0;
+-  double avgdum,sddum,rdum;
++  double sddum, rdum;	// Unused avgdum
+   unsigned char **pxl_tmp;
+   vector<img>::iterator it_img,it_img1;
+   string sdum=(tag2=="")?img0->tag:tag2; 
+@@ -1508,7 +1508,8 @@ void mergeImg(img *img0, img *img1, string mode,string
+   if ((img1->rows!=img0->rows)||(img1->columns!=img0->columns))
+     error_dodri("Size of two images do not match.");
+ 
+-  avgdum=sddum=0; 
++  // Unused avgdum=0
++  sddum=0; 
+   if (tag2=="") tag2=img0->tag; // merge into img1 by default
+   it_img= findImg(tag2);
+   if (it_img != vimg.end()) {
+@@ -1537,7 +1538,7 @@ void mergeImg(img *img0, img *img1, string mode,string
+ 	if (k>255) k=255;
+ 	if (k<0) k=abs(k);
+ 	it_img->pxl_gray[i][j]=(unsigned char)k;
+-	rdum=(double)k; avgdum+=rdum; sddum+=rdum*rdum;
++	rdum=(double)k; sddum+=rdum*rdum; // Unused avgdum+=rdum;
+       }
+     }
+     cout<<"    Image "<<tag2<<" modified."<<endl;
