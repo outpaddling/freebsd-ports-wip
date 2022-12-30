bfa022bfabb9d55bd01c9ab769425289d38d8962
Jason Bacon <jwb@FreeBSD.org>
2022-12-29 18:04:59 -0600

    Remove obsolete ports

--- tophat2012/files/patch-src-bam_merge.cpp
+++
@@ -1,7 +0,0 @@
---- src/bam_merge.cpp.orig	2014-05-12 12:49:52 UTC
-+++ src/bam_merge.cpp
-@@ -1,3 +1,4 @@
-+#include <getopt.h>
- #include "bam_merge.h"
- 
- #define USAGE "Usage: bam_merge [-Q] <out.bam> <in1.bam> <in2.bam> [...]\n"
