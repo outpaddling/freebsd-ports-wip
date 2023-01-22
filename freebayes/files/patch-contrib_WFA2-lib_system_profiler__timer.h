ad468de6a51c787a12ccbfea1fc27fe726f073d8
Jason Bacon <jwb@FreeBSD.org>
2023-01-22 10:53:23 -0600

    vcflib: Patch for 12.3-RELEASE, include time.h

---
+++ freebayes/files/patch-contrib_WFA2-lib_system_profiler__timer.h
@@ -0,0 +1,10 @@
+--- contrib/WFA2-lib/system/profiler_timer.h.orig	2023-01-22 16:48:45 UTC
++++ contrib/WFA2-lib/system/profiler_timer.h
+@@ -33,6 +33,7 @@
+ #ifndef PROFILER_TIMER_H
+ #define PROFILER_TIMER_H
+ 
++#include <time.h>
+ #include "profiler_counter.h"
+ 
+ /*
