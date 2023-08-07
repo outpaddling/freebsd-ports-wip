1f0d4c2e64645316aba6b0f730af497543bc3290
Jason Bacon <jwb@FreeBSD.org>
2023-08-07 08:32:45 -0500

    sra-tools: sratools.cpp: Add FREEBSD case
    main() is only defined for explicitly listed platforms

---
+++ sra-tools/files/patch-tools_external_driver-tool_sratools.cpp
@@ -0,0 +1,17 @@
+--- tools/external/driver-tool/sratools.cpp.orig	2023-07-10 16:23:35 UTC
++++ tools/external/driver-tool/sratools.cpp
+@@ -585,6 +585,14 @@ int main(int argc, char *argv[], char *envp[], char *a
+ }
+ #endif
+ 
++#ifdef FREEBSD
++int main(int argc, char *argv[], char *envp[])
++{
++    auto const invocation = CommandLine(argc, argv, envp, nullptr);
++    return sratools::main(invocation);
++}
++#endif
++
+ #if LINUX
+ int main(int argc, char *argv[], char *envp[])
+ {
