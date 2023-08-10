f3a4332da89acba16d910431fd5a6a48e897552c
Jason Bacon <jwb@FreeBSD.org>
2023-08-10 08:07:30 -0500

    sra-tools: Document executable path hack and reinplace PREFIX

--- sra-tools/files/patch-tools_external_driver-tool_file-path.posix.cpp
+++ sra-tools/files/patch-tools_external_driver-tool_file-path.posix.cpp
@@ -1,15 +1,18 @@
 --- tools/external/driver-tool/file-path.posix.cpp.orig	2023-07-10 16:23:35 UTC
 +++ tools/external/driver-tool/file-path.posix.cpp
-@@ -193,7 +193,13 @@ static char const *find_executable_path(char const *co
+@@ -193,7 +193,16 @@ static char const *find_executable_path(char const *co
  FilePath FilePath::fullPathToExecutable(char const *const *const argv, char const *const *const envp, char const *const *const extra)
  {
      char const *path;
 -#if LINUX
-+// FIXME: Temporary hack for testing
-+// /usr/local/bin should be replaced by actual installation prefix
++// Assume sra-tools is installed by FreeBSD ports.  FreeBSD users
++// don't often install software by other means.  Default prefix
++// is /usr/local, but this can be overridden by the user building
++// the port.  The port can replace /usr/local using sed if that's the
++// case.
 +#if BSD
-+    char full_path[1024];
-+    snprintf(full_path, 1024, "/usr/local/bin/%s", argv[0]);
++    char full_path[PATH_MAX];
++    snprintf(full_path, PATH_MAX, "/usr/local/bin/%s", argv[0]);
 +    path = full_path;
 +#elif LINUX
      path = "/proc/self/exe";
