78bd636ce51723f4fa3f83eab0b3e00afd669022
Jason Bacon <jwb@FreeBSD.org>
2023-08-11 09:01:47 -0500

    sra-tools: Make all patches upstreamable

--- sra-tools/files/patch-ncbi-vdb_test_kapp_run-sig-core.sh
+++ sra-tools/files/patch-ncbi-vdb_test_kapp_run-sig-core.sh
@@ -1,30 +1,27 @@
---- ncbi-vdb/test/kapp/run-sig-core.sh.orig	2017-10-27 07:23:08 UTC
+--- ncbi-vdb/test/kapp/run-sig-core.sh.orig	2023-08-11 13:37:24 UTC
 +++ ncbi-vdb/test/kapp/run-sig-core.sh
-@@ -29,12 +29,12 @@ BUILD_TYPE="$3"
+@@ -27,6 +27,7 @@ BINARY_PATH="$1"
+ HOST_OS="$2"
+ BUILD_TYPE="$3"
  
++# FIXME: Is this necessary?  Why not just use kill -s QUIT or kill -QUIT?
  SIGQUIT=3
  
--function killFromBackground ()
-+killFromBackground ()
- {
+ function killFromBackground ()
+@@ -34,7 +35,11 @@ function killFromBackground ()
      PARENT_PID=$1
      for I in 1 2 3 4 5
      do
 -        COMMAND="ps -ef|awk '\$3==${PARENT_PID} && \$8==\"${BINARY_PATH}\"  {print \$2}'"
-+        COMMAND="ps awwo pid,ppid,command|awk '\$2==${PARENT_PID} && \$3==\"${BINARY_PATH}\"  {print \$1}'"
++        if [ "$HOST_OS" = "bsd" ]; then
++            COMMAND="ps awwo pid,ppid,command|awk '\$2==${PARENT_PID} && \$3==\"${BINARY_PATH}\"  {print \$1}'"
++        else
++            COMMAND="ps -ef|awk '\$3==${PARENT_PID} && \$8==\"${BINARY_PATH}\"  {print \$2}'"
++        fi
          eval PID=\`${COMMAND}\`
          if [ "$PID" = "" ]; then
              sleep 1
-@@ -44,7 +44,7 @@ function killFromBackground ()
-     done
- 
-     if [ "$PID" != "" ]; then
--        kill -s $SIGQUIT $PID
-+        kill -s QUIT $PID
-     fi
- 
-     echo $PID > killed.pid
-@@ -63,6 +63,13 @@ if [ "$HOST_OS" = "mac" ]; then
+@@ -63,6 +68,13 @@ if [ "$HOST_OS" = "mac" ]; then
     fi
  
     CORE_FOLDER="/core/"
@@ -38,12 +35,16 @@
  elif [ "$HOST_OS" = "linux" ]; then
     if [ "`ulimit -c`" = "0" ]; then
         echo "Core files are disabled. Skipping core file tests"
-@@ -92,7 +99,7 @@ wait
+@@ -92,7 +104,11 @@ wait
  BINARY_PID=`cat killed.pid`
  rm killed.pid
  
 -CORE_FILE="${CORE_FOLDER}core.${BINARY_PID}"
-+CORE_FILE="${CORE_FOLDER}${BINARY_PATH##*/}.core"
++if [ "$HOST_OS" = "bsd" ]; then
++    CORE_FILE="${CORE_FOLDER}${BINARY_PATH##*/}.core"
++else
++    CORE_FILE="${CORE_FOLDER}core.${BINARY_PID}"
++fi
  
  if [ "$BUILD_TYPE" = "dbg" ]; then
     if [ -f $CORE_FILE ]; then
