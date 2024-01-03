21ab83789b900673f2d758390b9ca036f9f90e73
Jason Bacon <jwb@FreeBSD.org>
2024-01-02 20:25:45 -0600

    py-joblib: Clean up BSD support patch

--- py-joblib/files/patch-joblib_externals_loky_backend_context.py
+++ py-joblib/files/patch-joblib_externals_loky_backend_context.py
@@ -8,18 +8,13 @@
  import math
  import subprocess
  import traceback
-@@ -274,6 +275,14 @@ def _count_physical_cores():
+@@ -274,6 +275,9 @@ def _count_physical_cores():
              )
              cpu_info = cpu_info.stdout
              cpu_count_physical = int(cpu_info)
 +        # Maybe also openbsd, dragonfly, etc?
 +        elif sys.platform.startswith('freebsd'):
 +            cpu_count_physical = int(sysctl.filter("kern.smp.cores")[0].value);
-+            print('joblib: cpu_count = ' + str(cpu_count_physical), file=sys.stderr);
-+        elif sys.platform.startswith('netbsd'):
-+            # FIXME: This may report hyperthreads rather than cores
-+            cpu_count_physical = int(sysctl.filter("hw.ncpu")[0].value);
-+            print('joblib: cpu_count = ' + str(cpu_count_physical), file=sys.stderr);
          else:
              raise NotImplementedError(f"unsupported platform: {sys.platform}")
  
