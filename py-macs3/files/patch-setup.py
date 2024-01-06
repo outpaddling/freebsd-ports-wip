470836e2d27eff4eff4b801cd46bc378781cfbfd
Jason Bacon <jwb@FreeBSD.org>
2024-01-06 09:14:13 -0600

    py-macs3: Patch to work with cython < 3.0

--- py-macs3/files/patch-setup.py
+++ py-macs3/files/patch-setup.py
@@ -1,5 +1,14 @@
---- setup.py.orig	2023-12-29 23:02:00 UTC
+--- setup.py.orig	2023-11-15 16:09:23 UTC
 +++ setup.py
+@@ -40,7 +40,7 @@ classifiers =[\
+ install_requires = [ "numpy>=1.19",
+                      "hmmlearn>=0.3",
+                      "cykhash>=2.0,<3.0",
+-                     "Cython~=3.0" ]
++                     "Cython>=0" ]
+ 
+ tests_requires = [ 'pytest' ]
+ 
 @@ -70,7 +70,7 @@ def main():
  
      if not clang:
