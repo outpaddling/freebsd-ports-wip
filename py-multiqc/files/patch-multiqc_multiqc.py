d9885042e4357aa87d4a47336c6fed65a0ff26fe
Jason Bacon <jwb@FreeBSD.org>
2024-03-16 07:10:15 -0500

    py-multiqc: Update to 1.21 setuptools_scm -> setuptools-scm

--- py-multiqc/files/patch-multiqc_multiqc.py
+++ py-multiqc/files/patch-multiqc_multiqc.py
@@ -1,8 +1,8 @@
---- multiqc/multiqc.py.orig	2023-05-11 13:19:10 UTC
+--- multiqc/multiqc.py.orig	2024-02-28 13:56:02 UTC
 +++ multiqc/multiqc.py
-@@ -24,7 +24,13 @@ from urllib.request import urlopen
- 
+@@ -24,7 +24,13 @@ import traceback
  import jinja2
+ import requests
  import rich
 -import rich_click as click
 +if 'LC_ALL' in os.environ and 'LANG' in os.environ:
@@ -12,6 +12,6 @@
 +    print('in your environment in order for the click module to function.')
 +    print('E.g. export LC_ALL=en_US.UTF-8 or setenv LC_ALL en_US.UTF-8')
 +    sys.exit()
+ from packaging import version
  from rich.syntax import Syntax
  
- from .plots import table
