f508846c5933065b1b3d8345ea662b33a1db1376
Jason Bacon <jwb@FreeBSD.org>
2024-08-22 08:32:20 -0500

    freecad: Link to gtest_main and gtest, attempt to fix unresolved symbols

--- freecad/files/patch-src_Gui_SoFCCSysDragger.cpp
+++ freecad/files/patch-src_Gui_SoFCCSysDragger.cpp
@@ -1,24 +1,33 @@
---- src/Gui/SoFCCSysDragger.cpp.orig	2024-03-20 18:17:18 UTC
+--- src/Gui/SoFCCSysDragger.cpp.orig	2024-08-22 11:50:13 UTC
 +++ src/Gui/SoFCCSysDragger.cpp
-@@ -94,7 +94,7 @@ TDragger::TDragger()
+@@ -96,7 +96,7 @@ TDragger::TDragger()
  {
      SO_KIT_CONSTRUCTOR(TDragger);
  
--#if defined(Q_OS_MAC)
-+#if defined(Q_OS_MAC) || defined(Q_OS_FREEBSD)
+-#if defined(Q_OS_MACOS)
++#if defined(Q_OS_MACOS) || defined(Q_OS_FREEBSD)
      this->ref();
  #endif
  
-@@ -696,7 +696,7 @@ RDragger::RDragger()
+@@ -400,7 +400,7 @@ TPlanarDragger::TPlanarDragger()
+ TPlanarDragger::TPlanarDragger()
+ {
+     SO_KIT_CONSTRUCTOR(TPlanarDragger);
+-#if defined(Q_OS_MACOS)
++#if defined(Q_OS_MACOS) || defined(Q_OS_FREEBSD)
+     this->ref();
+ #endif
+ 
+@@ -698,7 +698,7 @@ RDragger::RDragger()
  RDragger::RDragger()
  {
      SO_KIT_CONSTRUCTOR(RDragger);
--#if defined(Q_OS_MAC)
-+#if defined(Q_OS_MAC) || defined(Q_OS_FREEBSD)
+-#if defined(Q_OS_MACOS)
++#if defined(Q_OS_MACOS) || defined(Q_OS_FREEBSD)
      this->ref();
  #endif
  
-@@ -1005,6 +1005,10 @@ SoFCCSysDragger::SoFCCSysDragger()
+@@ -1007,6 +1007,10 @@ SoFCCSysDragger::SoFCCSysDragger()
      : axisScale(1.0f, 1.0f, 1.0f)
  {
      SO_KIT_CONSTRUCTOR(SoFCCSysDragger);
@@ -27,5 +36,5 @@
 +    this->ref();
 +#endif
  
-     SO_KIT_ADD_CATALOG_ENTRY(annotation, SoAnnotation, TRUE, geomSeparator, "", TRUE);
+     SO_KIT_ADD_CATALOG_ENTRY(annotation, So3DAnnotation, TRUE, geomSeparator, "", TRUE);
      SO_KIT_ADD_CATALOG_ENTRY(scaleNode, SoScale, TRUE, annotation, "", TRUE);
