55daab76243fffac135ee39380fd8be232f76e93
Jason Bacon <jwb@FreeBSD.org>
2024-08-22 15:17:05 -0500

    freecad: Regenerate patches to silence portlint

--- freecad/files/patch-src_Gui_SoFCSelection.h
+++ freecad/files/patch-src_Gui_SoFCSelection.h
@@ -1,5 +1,5 @@
---- ./src/Gui/SoFCSelection.h.orig	2023-01-21 22:41:40.507560000 +0100
-+++ ./src/Gui/SoFCSelection.h	2023-01-21 22:42:27.657720000 +0100
+--- src/Gui/SoFCSelection.h.orig	2024-08-22 20:14:21 UTC
++++ src/Gui/SoFCSelection.h
 @@ -35,6 +35,8 @@
  # include <GL/gl.h>
  # endif
--- freecad/files/patch-src_Gui_SoFCUnifiedSelection.h
+++ freecad/files/patch-src_Gui_SoFCUnifiedSelection.h
@@ -1,5 +1,5 @@
---- src/Gui/SoFCUnifiedSelection.h.orig	2023-01-21 23:07:09.710205000 +0100
-+++ src/Gui/SoFCUnifiedSelection.h	2023-01-21 23:07:38.508972000 +0100
+--- src/Gui/SoFCUnifiedSelection.h.orig	2024-08-22 20:14:21 UTC
++++ src/Gui/SoFCUnifiedSelection.h
 @@ -23,6 +23,7 @@
  #ifndef GUI_SOFCUNIFIEDSELECTION_H
  #define GUI_SOFCUNIFIEDSELECTION_H
