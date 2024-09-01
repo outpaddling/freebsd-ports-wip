0a5105d5c58e26ad2a1028416b8c2dfb728ed840
Jason Bacon <jwb@FreeBSD.org>
2024-09-01 16:42:49 -0500

    freecad: Update to latest commit, add debug code for Gihub issue

--- freecad/files/patch-src_Gui_QuantitySpinBox.cpp
+++ freecad/files/patch-src_Gui_QuantitySpinBox.cpp
@@ -1,14 +1,14 @@
---- src/Gui/QuantitySpinBox.cpp.orig	2024-09-01 11:53:25 UTC
+--- src/Gui/QuantitySpinBox.cpp.orig	2024-09-01 17:20:00 UTC
 +++ src/Gui/QuantitySpinBox.cpp
 @@ -441,6 +441,12 @@ void QuantitySpinBox::updateEdit(const QString& text)
  
      edit->setText(text);
  
 +    // Debug issue #16116
-+    Base::Console().Message("QLineEdit, size=%i", edit->displayText().size());
-+    Base::Console().Message("QLineEdit, text=%s", edit->displayText().text());
-+    Base::Console().Message("d mystery variable, size=%i", d->unitStr().size());
-+    Base::Console().Message("d mystery variable, text=%s", d->unitStr());
++    Base::Console().Message("QLineEdit, size=%i\n", edit->displayText().size());
++    Base::Console().Message("QLineEdit, text=%s\n", edit->displayText().toStdString().c_str());
++    Base::Console().Message("d mystery variable, size=%i\n", d->unitStr.size());
++    Base::Console().Message("d mystery variable, text=%s\n", d->unitStr.toStdString().c_str());
 +
      cursor = qBound(0, cursor, edit->displayText().size() - d->unitStr.size());
      if (selsize > 0) {
