c5169036f17c7ce0dcef51589aa59c8843bdc12f
Jason Bacon <jwb@FreeBSD.org>
2024-09-02 07:20:30 -0500

    freecad: Add hack to prevent crash when dim coordinate is 0

--- freecad/files/patch-src_Gui_QuantitySpinBox.cpp
+++ freecad/files/patch-src_Gui_QuantitySpinBox.cpp
@@ -1,15 +1,18 @@
 --- src/Gui/QuantitySpinBox.cpp.orig	2024-09-01 17:20:00 UTC
 +++ src/Gui/QuantitySpinBox.cpp
-@@ -441,6 +441,12 @@ void QuantitySpinBox::updateEdit(const QString& text)
+@@ -441,7 +441,14 @@ void QuantitySpinBox::updateEdit(const QString& text)
  
      edit->setText(text);
  
+-    cursor = qBound(0, cursor, edit->displayText().size() - d->unitStr.size());
 +    // Debug issue #16116
 +    Base::Console().Message("QLineEdit, size=%i\n", edit->displayText().size());
 +    Base::Console().Message("QLineEdit, text=%s\n", edit->displayText().toStdString().c_str());
 +    Base::Console().Message("d mystery variable, size=%i\n", d->unitStr.size());
 +    Base::Console().Message("d mystery variable, text=%s\n", d->unitStr.toStdString().c_str());
 +
-     cursor = qBound(0, cursor, edit->displayText().size() - d->unitStr.size());
++    int size = edit->displayText().size() - d->unitStr.size();
++    cursor = qBound(0, cursor, size < 0 ? 0 : size);
      if (selsize > 0) {
          edit->setSelection(0, cursor);
+     }
