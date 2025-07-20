e43e07efbe9a9d840df77f0852309a7721bd9c9c
Jason Bacon <jwb@FreeBSD.org>
2025-07-20 07:26:04 -0500

    lumina-core: Add missing "" for empty translation types

---
+++ lumina-core/files/patch-lumina-desktop_i18n_lumina-desktop__sv.ts
@@ -0,0 +1,20 @@
+--- lumina-desktop/i18n/lumina-desktop_sv.ts.orig	2025-07-20 12:21:18 UTC
++++ lumina-desktop/i18n/lumina-desktop_sv.ts
+@@ -85,7 +85,7 @@
+     <message>
+         <location filename="../desktop-plugins/applauncher/AppLauncherPlugin.cpp" line="156"/>
+         <source>Drag to Application</source>
+-        <translation type=>Drag till program</translation>
++        <translation type="">Drag till program</translation>
+     </message>
+     <message>
+         <location filename="../desktop-plugins/applauncher/AppLauncherPlugin.cpp" line="217"/>
+@@ -853,7 +853,7 @@
+     <message>
+         <location filename="../src-screensaver/LLockScreen.ui" line="114"/>
+         <source>Password</source>
+-        <translation type=>Lösenord</translation>
++        <translation type="">Lösenord</translation>
+     </message>
+     <message>
+         <location filename="../src-screensaver/LLockScreen.ui" line="126"/>
